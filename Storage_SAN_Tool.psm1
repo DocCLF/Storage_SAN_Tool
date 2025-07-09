aa4df318229adf92add9e06e66913c886abee5ea
ToolDoc <mailtodoc@web.de>
2025-07-09 15:28:21 +0200

    Update Storage_SAN_Tool.psm1

--- Storage_SAN_Tool.psm1
+++ Storage_SAN_Tool.psm1
@@ -6,6 +6,8 @@ The Unblock-File cmdlet lets you open files that were downloaded from the intern
 It unblocks PowerShell script files that were downloaded from the internet so you can run them, even when the PowerShell execution policy is RemoteSigned. 
 By default, these files are blocked to protect the computer from untrusted files.
 #>
+    Unblock-File -Path $PSScriptRoot\TOOLFunc\DashBoard\*.ps1 -Confirm:$false
+    $DashBoard_Functions = @(Get-ChildItem -Path $PSScriptRoot\TOOLFunc\DashBoard\*.ps1 -ErrorAction SilentlyContinue)
     Unblock-File -Path $PSScriptRoot\TOOLFunc\*.ps1 -Confirm:$false
     $TOOL_Functions = @(Get-ChildItem -Path $PSScriptRoot\TOOLFunc\*.ps1 -ErrorAction SilentlyContinue)
     Unblock-File -Path $PSScriptRoot\USER\*.ps1 -Confirm:$false
@@ -23,7 +25,7 @@ By default, these files are blocked to protect the computer from untrusted files
 
     $FoundErrors = @(
 
-        foreach($import in @($TOOL_Functions + $USER_Functions + $IBMPower_Functions + $IBMStorage_Functions + $FOSBrocade_Functions + $HealthCheck_Functions + $GUI_Functions)) {
+        foreach($import in @($DashBoard_Functions + $TOOL_Functions + $USER_Functions + $IBMPower_Functions + $IBMStorage_Functions + $FOSBrocade_Functions + $HealthCheck_Functions + $GUI_Functions)) {
             try {
                . $import.fullname
             }
