8eb7236d268a001789b6f53b844342c282d8a8a2
ToolDoc <mailtodoc@web.de>
2025-07-15 10:29:30 +0200

    Update SST_DashBoardMain.ps1

--- TOOLFunc/DashBoard/SST_DashBoardMain.ps1
+++ TOOLFunc/DashBoard/SST_DashBoardMain.ps1
@@ -1,7 +1,9 @@
 function SST_DashBoardMain {
     [CmdletBinding()]
     param (
-        [string]$MainPath
+        [string]$MainPath,
+        $SST_SQLiteSTODashBoardQuery = $null,
+        $SST_SQLiteDBReader = $null
     )
     
     begin {
@@ -17,15 +19,46 @@ function SST_DashBoardMain {
     
     process {
         try {
-        $SST_SQliteReadCMD.CommandText = "SELECT ID, DID, Name, WWNN, Status, IOgroupid, IOgroupName, SerialNumber, CodeLevel, ConfigNode, SideID, SideName, ProdMTM, TimeStamp FROM IBMSTOHWTable;"
-        $SST_SQLiteDBReader = $SST_SQliteReadCMD.ExecuteReader()
+            $SST_SQLiteSTODashBoardQuery = $null
+            $SST_SQLiteSTODashBoardQuery = " SELECT ID, DID, Name, WWNN, Status, IOgroupid, IOgroupName, SerialNumber, CodeLevel, ConfigNode, SideID, SideName, ProdMTM, TimeStamp FROM IBMSTOHWTable d WHERE TimeStamp = ( SELECT MAX(TimeStamp) FROM IBMSTOHWTable WHERE DID = d.DID ) ORDER BY DID; "
+            $SST_SQliteReadCMD.CommandText = $SST_SQLiteSTODashBoardQuery
+            $SST_SQLiteDBReader = $SST_SQliteReadCMD.ExecuteReader()
 
-        SST_DashBoardSTO -STOHWCollection $SST_SQLiteDBReader
+            SST_DashBoardSTO -STOHWCollection $SST_SQLiteDBReader
+            $SST_SQLiteDBReader.Close()
         }
         catch {
+            Write-Host $_.Exception.Message
             SST_ToolMessageCollector -TD_ToolMSGCollector "There is something wrong, mybe there is no Table" -TD_ToolMSGType Warning -TD_Shown yes
         }
 
+        try {
+            $SST_SQLiteSTODashBoardQuery = $null
+            $SST_SQLiteSTODashBoardQuery = " SELECT ID, HID, Name, Status, HostClusterName, SideName, TimeStamp FROM IBMSTOHostTable d WHERE TimeStamp = ( SELECT MAX(TimeStamp) FROM IBMSTOHostTable WHERE HID = d.HID ) AND Status != 'online' ORDER BY HID; "
+            $SST_SQliteReadCMD.CommandText = $SST_SQLiteSTODashBoardQuery
+            $SST_SQLiteDBReader = $SST_SQliteReadCMD.ExecuteReader()
+
+            SST_DashBoardHosts -STOHWCollection $SST_SQLiteDBReader
+            $SST_SQLiteDBReader.Close()
+        }
+        catch {
+            Write-Host $_.Exception.Message
+            SST_ToolMessageCollector -TD_ToolMSGCollector "There is something wrong, mybe there is no Table" -TD_ToolMSGType Warning -TD_Shown yes
+        }
+
+        try {
+            $SST_SQLiteSTODashBoardQuery = $null
+            $SST_SQLiteSTODashBoardQuery = " SELECT Name, Status, CodeLevel, BrocadeProdName, SerialNumber, TimeStamp FROM IBMSANHWTable d WHERE TimeStamp = ( SELECT MAX(TimeStamp) FROM IBMSANHWTable WHERE SerialNumber = d.SerialNumber ) ORDER BY SerialNumber; "
+            $SST_SQliteReadCMD.CommandText = $SST_SQLiteSTODashBoardQuery
+            $SST_SQLiteDBReader = $SST_SQliteReadCMD.ExecuteReader()
+
+            SST_DashBoardSAN -STOHWCollection $SST_SQLiteDBReader
+            $SST_SQLiteDBReader.Close()
+        }
+        catch {
+            Write-Host $_.Exception.Message
+            SST_ToolMessageCollector -TD_ToolMSGCollector "There is something wrong, mybe there is no Table" -TD_ToolMSGType Warning -TD_Shown yes
+        }
     }
     
     end {
