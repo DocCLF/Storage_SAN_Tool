2a3de422f42de33ce9afc3f9de59db4c691ee305
ToolDoc <mailtodoc@web.de>
2025-07-22 19:41:08 +0200

    Update SST_DashBoardDrives.ps1

--- TOOLFunc/DashBoard/SST_DashBoardDrives.ps1
+++ TOOLFunc/DashBoard/SST_DashBoardDrives.ps1
@@ -25,31 +25,31 @@ function SST_DashBoardDrives {
                     $TD_TB_NKNResOne.Text = "$FWlev / $LatestDriveFW"
                     $TD_TB_NKNDescrOne.Visibility = "visible"
                     $TD_TB_NKNResOne.Visibility = "visible"
-                    #if("$FWlev" -like "*$LatestDriveFW*"){
-                    #    $TD_TB_NKNResOne.Foreground = "Green"
-                    #}else {
-                    #    $TD_TB_NKNResOne.Foreground = "Orange"
-                    #}
+                    if($LatestDriveFW -like "*$FWlev*"){
+                        $TD_TB_NKNResOne.Foreground = "Green"
+                    }else {
+                        $TD_TB_NKNResOne.Foreground = "Orange"
+                    }
                 }elseif ($TD_TB_NKNResTwo.Text -eq "") {
                     $TD_TB_NKNDescrTwo.Text = "$Name Drive FW:"
                     $TD_TB_NKNResTwo.Text = "$FWlev / $LatestDriveFW"
                     $TD_TB_NKNDescrTwo.Visibility = "visible"
                     $TD_TB_NKNResTwo.Visibility = "visible"
-                    #if($FWlev -eq $LatestDriveFW){
-                    #    $TD_TB_NKNResTwo.Foreground = "Green"
-                    #}else {
-                    #    $TD_TB_NKNResTwo.Foreground = "Orange"
-                    #}
+                    if($LatestDriveFW -like "*$FWlev*"){
+                        $TD_TB_NKNResTwo.Foreground = "Green"
+                    }else {
+                        $TD_TB_NKNResTwo.Foreground = "Orange"
+                    }
                 }else{
                     $TD_TB_NKNDescrThree.Text = "$Name Drive FW:"
                     $TD_TB_NKNResThree.Text = "$FWlev / $LatestDriveFW"
                     $TD_TB_NKNDescrThree.Visibility = "visible"
                     $TD_TB_NKNResThree.Visibility = "visible"
-                    #if($FWlev -eq $LatestDriveFW){
-                    #    $TD_TB_NKNResThree.Foreground = "Green"
-                    #}else {
-                    #    $TD_TB_NKNResThree.Foreground = "Orange"
-                    #}
+                    if($LatestDriveFW -like "*$FWlev*"){
+                        $TD_TB_NKNResThree.Foreground = "Green"
+                    }else {
+                        $TD_TB_NKNResThree.Foreground = "Orange"
+                    }
                 }
                 $ProductIDOld = $ProductID
             }
