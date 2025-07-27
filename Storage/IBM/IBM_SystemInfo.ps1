1ac308cb22c37d96f7ae5c2a84f35099953400f6
ToolDoc <mailtodoc@web.de>
2025-07-27 16:40:30 +0200

    Update IBM_SystemInfo.ps1

--- Storage/IBM/IBM_SystemInfo.ps1
+++ Storage/IBM/IBM_SystemInfo.ps1
@@ -16,18 +16,12 @@ function IBM_SystemInfo {
         #<# suppresses error messages #>
         $ErrorActionPreference="SilentlyContinue"
 
-        
-
-        #if($TD_Device_ConnectionTyp -eq "ssh"){
-        #    $TD_DeviceInformation = ssh -i $($TD_Device_SSHKeyPath) $TD_Device_UserName@$TD_Device_DeviceIP 'lssystem -delim :'
-        #}else {
-            $TD_DeviceInformation = plink $TD_Device_UserName@$TD_Device_DeviceIP -pw $TD_Device_PW -batch 'lssystem -delim :'
-        #}
+        $TD_DeviceInformation = plink $TD_Device_UserName@$TD_Device_DeviceIP -pw $TD_Device_PW -batch 'lssystem -delim :'
         
     }
     
     process {
-        $TD_DeviceName = (($TD_DeviceInformation | Select-String -Pattern 'name:(\w+)' -AllMatches).Matches.groups[1].Value)
+
         $IBM_STOSysInfos =[ordered]@{}
         $IBM_STOSysInfos.Add('MDiskTotalCapacity',(($TD_DeviceInformation | Select-String -Pattern 'total_mdisk_capacity:([\w\.]+)' -AllMatches).Matches.groups[1].Value))
         $IBM_STOSysInfos.Add('MDiskFreeCapacity',(($TD_DeviceInformation | Select-String -Pattern 'total_free_space:([\w\.]+)' -AllMatches).Matches.groups[1].Value))
@@ -37,9 +31,6 @@ function IBM_SystemInfo {
         $IBM_STOSysInfos.Add('HostUnmap',(($TD_DeviceInformation | Select-String -Pattern 'host_unmap:(on|off)' -AllMatches).Matches.groups[1].Value))
         $IBM_STOSysInfos.Add('BackendUnmap',(($TD_DeviceInformation | Select-String -Pattern 'backend_unmap:(on|off)' -AllMatches).Matches.groups[1].Value))
 
-        <# Progressbar  #>
-        
-        
     }
     
     end {
