9f08110ad5ae2f83a814ada3ac106862670c99aa
ToolDoc <mailtodoc@web.de>
2025-08-07 11:59:11 +0200

    Update SST_DashBoardSAN.ps1

--- TOOLFunc/DashBoard/SST_DashBoardSAN.ps1
+++ TOOLFunc/DashBoard/SST_DashBoardSAN.ps1
@@ -1,53 +1,36 @@
 function SST_DashBoardSAN {
     [CmdletBinding()]
     param (
-        $STOHWCollection
+        $SANHWCollection,
+        $SQLReader
     )
     
     begin {
-        $TD_BTN_SAN_DevOne,$TD_BTN_SAN_DevTwo,$TD_BTN_SAN_DevThree,$TD_BTN_SAN_DevFour,$TD_BTN_SAN_DevFive,$TD_BTN_SAN_DevSix,$TD_BTN_SAN_DevSeven,$TD_BTN_SAN_DevEight |ForEach-Object {$_.Visibility = "Collapsed"}
+        $DeviceCounter = 0
+        $SQLReader.CommandText = $SANHWCollection
+        $SST_SQLiteDBReader = $SQLReader.ExecuteReader()
     }
     
     process {
 
         #ID, DID, Name, Status, CodeLevel, BrocadeProdName, SerialNumber, TimeStamp FROM IBMSANHWTabl
-        $DeviceCounter = 0
-        while ($STOHWCollection.Read()) {
-            $Name  = $STOHWCollection["Name"]
-            $Status    = $STOHWCollection["Status"]
-            $CodeLevel  = $STOHWCollection["CodeLevel"]
-            $BrocadeProdName = $STOHWCollection["BrocadeProdName"]
-            $SerialNumber    = $STOHWCollection["SerialNumber"]
-            $TimeStamp = $STOHWCollection["TimeStamp"]
-
-            if($SerialNumber -ne $SerialNumberOld){
-                $DeviceCounter++
-                $SerialNumberOld = $SerialNumber
+        while ($SST_SQLiteDBReader.Read()) {
+            $DeviceCounter++
+            $DashBoardSANsObj = [PSCustomObject]@{
+            Name  = $SST_SQLiteDBReader["Name"]
+            Status    = $SST_SQLiteDBReader["Status"]
+            CodeLevel  = $SST_SQLiteDBReader["CodeLevel"]
+            BrocadeProdName = $SST_SQLiteDBReader["BrocadeProdName"]
+            SerialNumber    = $SST_SQLiteDBReader["SerialNumber"]
+            TimeStamp = $SST_SQLiteDBReader["TimeStamp"]
+            BrocadeIcon = "$PSRootPath\Resources\icons\broadcom-96.png"
+            ClockIcon96 = "$PSRootPath\Resources\icons\icons8-clock-96.png"
             }
-
-            $SST_BTN_Content="Name: $Name`nStatus: $Status`nMTM:  $BrocadeProdName`nSN:      $SerialNumber`nFW:      $CodeLevel"
-            $TD_TB_SAN_DevOne,$TD_TB_SAN_DevTwo,$TD_TB_SAN_DevThree,$TD_TB_SAN_DevFour,$TD_TB_SAN_DevFive,$TD_TB_SAN_DevSix,$TD_TB_SAN_DevSeven,$TD_TB_SAN_DevEight |ForEach-Object {
-                $SST_CheckBTN_Content = $_.Text
-                if((!([string]::IsNullOrWhiteSpace($SST_BTN_Content))) -and ([string]::IsNullOrWhiteSpace($SST_CheckBTN_Content))){
-                    $_.Text = $SST_BTN_Content
-                    $_.Visibility = "Visible"
-                    $SST_BTN_Content = $null
-                    $TB_Name = $_.Name.TrimStart('TB_')
-                    $TD_TB_Clock_SAN_DevOne,$TD_TB_Clock_SAN_DevTwo,$TD_TB_Clock_SAN_DevThree,$TD_TB_Clock_SAN_DevFour,$TD_TB_Clock_SAN_DevFive,$TD_TB_Clock_SAN_DevSix,$TD_TB_Clock_SAN_DevSeven,$TD_TB_Clock_SAN_DevEight | ForEach-Object {
-                        $TB_ClockName = $_.Name.TrimStart('TB_Clock_')
-                        if($TB_Name -eq $TB_ClockName){
-                            $_.Text = $TimeStamp
-                        }
-                    }
-                    
-                    $TD_BTN_SAN_DevOne,$TD_BTN_SAN_DevTwo,$TD_BTN_SAN_DevThree,$TD_BTN_SAN_DevFour,$TD_BTN_SAN_DevFive,$TD_BTN_SAN_DevSix,$TD_BTN_SAN_DevSeven,$TD_BTN_SAN_DevEight |ForEach-Object {
-                        if($($_.Name) -like "*$TB_Name"){
-                            $_.Visibility = "Visible"
-                        }
-                    }
-                }
-            }
+            $DashBoardSANDeviceView.Add($DashBoardSANsObj)
         }
+        $SST_SQLiteDBReader.Close()
+        $TD_IC_DashBoardSANDevice.ItemsSource = $DashBoardSANDeviceView
+
         $TD_TB_SANDEVCount.Text = $DeviceCounter
     }
     
