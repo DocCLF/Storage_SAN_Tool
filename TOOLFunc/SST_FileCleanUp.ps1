78df75acdf0af0360bd8503512b3f156bae0ac26
ToolDoc <mailtodoc@web.de>
2025-08-08 21:34:00 +0200

    Update SST_FileCleanUp.ps1

--- TOOLFunc/SST_FileCleanUp.ps1
+++ TOOLFunc/SST_FileCleanUp.ps1
@@ -52,7 +52,7 @@ function SST_FileCleanUp {
             try {
                 $TD_FilesToDelete = (Get-ChildItem -Path $TD_UsedLogPath -Recurse -File | Where-Object { $_.LastWriteTime -lt $CutoffDate}).Count
                 
-                Get-ChildItem -Path $TD_UsedLogPath -Recurse -File | Where-Object { $_.LastWriteTime -lt $CutoffDate} | Remove-Item -Confirm:$false -Force -ErrorAction SilentlyContinue
+                Get-ChildItem -Path $TD_UsedLogPath -Attributes $TD_DeleteWhat -Exclude *.xaml -Recurse -File | Where-Object { $_.LastWriteTime -lt $CutoffDate} | Remove-Item -Confirm:$false -Force -ErrorAction SilentlyContinue
 
                 SST_ToolMessageCollector -TD_ToolMSGCollector "$TD_FilesToDelete files older than $TD_KeepFilesForDays days have been deleted from the directory: $TD_UsedLogPath " -TD_ToolMSGType Message -TD_Shown yes
             }
