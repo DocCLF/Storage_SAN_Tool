a4a0a4783803b5dba94b326942c94a4d66b3197e
ToolDoc <mailtodoc@web.de>
2025-08-15 15:56:09 +0200

    Update IBM_SystemInfo.ps1

--- Storage/IBM/IBM_SystemInfo.ps1
+++ Storage/IBM/IBM_SystemInfo.ps1
@@ -21,9 +21,19 @@ function IBM_SystemInfo {
     }
     
     process {
+        $MDiskTotalCapacityinTB = $null
+
+        $TempMDiskCap=(($TD_DeviceInformation | Select-String -Pattern 'total_mdisk_capacity:([\w\.]+)' -AllMatches).Matches.groups[1].Value)
+        if($TempMDiskCap -like "PB"){
+            [float]$TemPConvertCap=($TempMDiskCap -replace 'PB','')
+            [string]$TemPConvertCapToTB = [math]::Round($TemPConvertCap*1000)
+            [string]$MDiskTotalCapacityinTB = [string]$TemPConvertCapToTB + "TB"
+        }else {
+            $MDiskTotalCapacityinTB = $TempMDiskCap
+        }
 
         $IBM_STOSysInfos =[ordered]@{}
-        $IBM_STOSysInfos.Add('MDiskTotalCapacity',(($TD_DeviceInformation | Select-String -Pattern 'total_mdisk_capacity:([\w\.]+)' -AllMatches).Matches.groups[1].Value))
+        $IBM_STOSysInfos.Add('MDiskTotalCapacity',$MDiskTotalCapacityinTB)
         $IBM_STOSysInfos.Add('MDiskFreeCapacity',(($TD_DeviceInformation | Select-String -Pattern 'total_free_space:([\w\.]+)' -AllMatches).Matches.groups[1].Value))
         $IBM_STOSysInfos.Add('MDiskUsedCapacity',(($TD_DeviceInformation | Select-String -Pattern 'space_allocated_to_vdisks:([\w\.]+)' -AllMatches).Matches.groups[1].Value))
         $IBM_STOSysInfos.Add('PhysicalTotalCapacity',(($TD_DeviceInformation | Select-String -Pattern 'physical_capacity:([\w\.]+)' -AllMatches).Matches.groups[1].Value))
