bb2b8ba048ce3d60a47d485c46186adbd1019327
ToolDoc <mailtodoc@web.de>
2025-10-01 20:47:03 +0200

    Update SST_MainHealthCheckFunc.ps1

--- HealthCheck/SST_MainHealthCheckFunc.ps1
+++ HealthCheck/SST_MainHealthCheckFunc.ps1
@@ -70,6 +70,35 @@ function SST_MainHealthCheckFunc {
         catch {
             SST_ToolMessageCollector -TD_ToolMSGCollector $_.Exception.Message -TD_ToolMSGType Error -TD_Shown yes       
         }
+
+        try {
+            $TD_Credentials = $TD_DG_KnownDeviceList.ItemsSource |Where-Object {$_.DeviceTyp -eq "PowerHMC"}
+            SST_ToolMessageCollector -TD_ToolMSGCollector $TD_Credentials.DeviceTyp -TD_ToolMSGType Message -TD_Shown no
+            <# Find Warppanel in UC and add Button Stlye #>
+            $SST_STOHealthCheckWP = $SST_UCOBJ.FindName("WP_SVRHealthCheck")
+
+            foreach ($TD_Credential in $TD_Credentials) {
+
+                $SST_DummyBTN = SST_CreateButton -SST_UCOBJ $SST_UCOBJ -SST_UCSTYLEOBJ $SST_UCSTYLEOBJ -DeviceTyp $TD_Credential.DeviceTyp -DeviceID $TD_Credential.ID -DeviceIPAddress $TD_Credential.IPAddress
+                #$win = $SST_UCOBJ.FindName($SST_DummyBTN)
+
+                $SST_UCOBJ.RegisterName($SST_DummyBTN.Name, $SST_DummyBTN)
+
+                $SST_STOHealthCheckWP.Children.Add($SST_DummyBTN)
+
+                $SST_DummyBTN.Add_Click({ 
+                    param($sender,$e)
+                    $FoundUSControl = Get-ParentUserControl -control $sender
+                    $TD_Credential = $TD_DG_KnownDeviceList.ItemsSource |Where-Object {($_.DeviceTyp -eq "PowerHMC")-and($this.Name -like "*_$($_.ID)")}   
+
+                    IBM_SANHealthCheck -SST_DeviceLoggingInfo $TD_Credential -UCOBJ $FoundUSControl
+                })
+
+            }
+        }
+        catch {
+            SST_ToolMessageCollector -TD_ToolMSGCollector $_.Exception.Message -TD_ToolMSGType Error -TD_Shown yes       
+        }
     }
     
     end {
