da2cd6329279386b810c0cd04ff48bf361fa6b55
ToolDoc <mailtodoc@web.de>
2025-10-09 21:43:56 +0200

    Update IBM_PowerMainFunc.ps1

--- Server/IBMPower/IBM_PowerMainFunc.ps1
+++ Server/IBMPower/IBM_PowerMainFunc.ps1
@@ -6,20 +6,19 @@ function IBM_PowerMainFunc {
         $SST_UCOBJ,
         $SST_MWOBJ,
         $SST_UCSTYLEOBJ,
+        $SecureData,
+        $CloudString,
         [bool]$SST_Extension
     )
     
     begin {
         <# check at first is there any extension #>
-        $SST_Extension = SST_ExtensionChecker -SST_UCOBJ $SST_UCOBJ
-        if(!($SST_Extension)){
-            $SST_BTN_PowerBoard = $SST_UCOBJ.FindName("BTN_HMCCollector")
-            #$TD_BTN_HMCInfoView = $SST_UCOBJ.FindName("BTN_HMCInfoView")
-            $SST_BTN_PowerBoardTooltip = $SST_UCOBJ.FindName("BTN_HMCCollectorTooltip")
-            $SST_BTN_PowerBoard.Background = "Coral"
-            $SST_BTN_PowerBoardTooltip.Text = "No Extension are installed or Credentials are loaded!"
-            SST_ToolMessageCollector -TD_ToolMSGCollector "IBM_PowerMainFunc: No Extension are installed or Credentials are loaded!" -TD_ToolMSGType Warning -TD_Shown yes
+        if($SecureData.count -lt 1){
+            $SST_Extension = SST_ExtensionChecker -SST_UCOBJ $SST_UCOBJ
+        }else{
+            SST_ExtensionChecker -SST_UCOBJ $SST_UCOBJ -LoadedToolSettings $SecureData -CloudDB $true
         }
+  
         if(((SST_PowerDBFunc -MainPath $PSRootPath -IBMPowerOperatorDB "CountHMCTabelleEntrys") -ge 1)){
 
             SST_PowerDBFunc -MainPath $PSRootPath -IBMPowerOperatorDB "HMCGUIInfo" -UCOBJ $SST_UCOBJ
