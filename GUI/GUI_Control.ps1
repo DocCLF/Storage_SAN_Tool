7e9cebc637d38c6e9ac25faa1f87238b6d27608b
ToolDoc <mailtodoc@web.de>
2025-10-15 10:47:39 +0200

    Update GUI_Control.ps1

--- GUI/GUI_Control.ps1
+++ GUI/GUI_Control.ps1
@@ -1266,6 +1266,16 @@ $TD_btn_FOS_SwitchShow.add_click({
 
     $TD_Credentials | ForEach-Object {
         [array]$FOS_SwitchShow = FOS_SwitchShowInfo -TD_Line_ID $_.ID -TD_Device_ConnectionTyp $_.ConnectionTyp -TD_Device_UserName $_.UserName -TD_Device_DeviceName $_.DeviceName -TD_Device_DeviceIP $_.IPAddress -TD_Device_PW $([Net.NetworkCredential]::new('', $_.Password).Password) -TD_Device_SSHKeyPath $_.SSHKeyPath -TD_Exportpath $TD_tb_ExportPath.Text
+        
+        try {
+            SST_LiteDBControl -SST_InfoType "SANPortInfo" -SST_CollectedInformations $FOS_SwitchShow
+        }
+        catch {
+            <#Do this if a terminating exception happens#>
+            Write-Host $_.exception.message
+            SST_ToolMessageCollector -TD_ToolMSGCollector "LiteDB - $($_.exception.message)" -TD_ToolMSGType Error -TD_Shown no
+        }
+
         switch ($_.ID) {
             {($_ -eq 1)} { $TD_DG_SwitchShowOne.ItemsSource = $FOS_SwitchShow }
             {($_ -eq 2)} { $TD_DG_SwitchShowTwo.ItemsSource = $FOS_SwitchShow }
