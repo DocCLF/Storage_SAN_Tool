5404b61821da36411462669a84f5a9b0dd5584fe
ToolDoc <mailtodoc@web.de>
2025-10-18 07:02:05 +0200

    Update SST_PRISMDBControl.ps1

--- TOOLFunc/DBFunc/SST_PRISMDBControl.ps1
+++ TOOLFunc/DBFunc/SST_PRISMDBControl.ps1
@@ -24,10 +24,10 @@ function SST_PRISMDBControl {
                 while (!($SQLSVRCon)) {
                     if($SQLConnection.State -eq "open"){
                         $SQLSVRCon =$true
-                        SST_ToolMessageCollector -TD_ToolMSGCollector "SST_PRISMDBControl Func State $($SQLConnection.State)" -TD_ToolMSGType Message -TD_Shown yes
+                        SST_ToolMessageCollector -TD_ToolMSGCollector $("SST_PRISMDBControl Func State $($SQLConnection.State)") -TD_ToolMSGType Message -TD_Shown yes
                     }else {
                         $SQLConnection.Open()
-                        SST_ToolMessageCollector -TD_ToolMSGCollector "SST_PRISMDBControl Func State $($SQLConnection.State)" -TD_ToolMSGType Message -TD_Shown no
+                        SST_ToolMessageCollector -TD_ToolMSGCollector $("SST_PRISMDBControl Func State $($SQLConnection.State)") -TD_ToolMSGType Message -TD_Shown no
                     }
                 }
                 $CustomerNumber = $SST_LoadedToolSettings.CustomerNumber
@@ -35,10 +35,10 @@ function SST_PRISMDBControl {
                 $CustomerNumber =$null
                 $SST_LoadedToolSettings =$null
                 $ConnectionStringPRISM =$null
-                SST_ToolMessageCollector -TD_ToolMSGCollector "SST_PRISMDBControl Func there is a problem with the CustomerNumber" -TD_ToolMSGType Error -TD_Shown yes
+                SST_ToolMessageCollector -TD_ToolMSGCollector $("SST_PRISMDBControl Func there is a problem with the CustomerNumber") -TD_ToolMSGType Error -TD_Shown yes
             }
         }catch{
-            Write-Host $_.exception.message
+            SST_ToolMessageCollector -TD_ToolMSGCollector $("SST_PRISMDBControl Func there is a problem with $( $_.exception.message)") -TD_ToolMSGType Error -TD_Shown yes
         }
     }
     
@@ -53,24 +53,24 @@ function SST_PRISMDBControl {
                         $SST_CollectedInformations | ForEach-Object {
                             Start-Sleep -Seconds 0.5
                             $TD_SQLCommand.CommandText="INSERT INTO [dbo].[StorageBase] (STOName,STOClusterName,STOStatus,STOWWNN,STOProdMTM,STOSerialNumber,STOCodeLevel,STORecommendedPTF,STOIOgroupid,STOIOgroupName,STOConfigNode,STOSideID,STOSideName,STOMDiskTC,STOMDiskFC,STOMDiskUC,STOPhysicalTC,STOPhysicalFC,STOCustomerNumber,TimeStamp) VALUES ('$($_.Name)','$($_.ClusterName)','$($_.Status)','$($_.WWNN)','$($_.Prod_MTM)','$($_.Serial_Number)','$($_.Code_Level)','$($_.RecommendedPTF)','$($_.IO_group_id)','$($_.IO_group_Name)','$($_.ConfigNode)','$($_.SideID)','$($_.SideName)','$($_.'MDiskTotalCapacity')','$($_.'MDiskFreeCapacity')','$($_.'MDiskUsedCapacity')','$($_.'PhysicalTotalCapacity')','$($_.'PhysicalFreeCapacity')','$CustomerNumber','$TimeStamp');"
-                            Write-Host $TD_SQLCommand.ExecuteNonQuery() 
+                            Write-Debug $TD_SQLCommand.ExecuteNonQuery() 
                         }
                     }
                 "StorageDrive" { 
                         $SST_CollectedInformations | ForEach-Object {
                             Start-Sleep -Seconds 0.5 
                             $TD_SQLCommand.CommandText="INSERT INTO [dbo].[StorageDrive] (DriveID,DriveSlot,DriveProductID,DriveStatus,DriveCurrentFW,DriveLatestFW,DriveCap,DrivePhyCap,DrivePhyUsedCap,DriveEffeUsedCap,STOSN,STOWWNN,STOCustomerNumber,TimeStamp) VALUES ('$($_.DriveID)','$($_.Slot)','$($_.ProductID)','$($_.DriveStatus)','$($_.FWlev)','$($_.LatestDriveFW)','$($_.DriveCap)','$($_.PhyDriveCap)','$($_.PhyUsedDriveCap)','$($_.EffeUsedDriveCap)','$($_.DeviceSN)','$($_.DeviceWWNN)','$CustomerNumber','$TimeStamp');"    
-                            Write-Host $TD_SQLCommand.ExecuteNonQuery() 
+                            Write-Debug $TD_SQLCommand.ExecuteNonQuery() 
                         }
                     }
                 "SANBase" { 
                         $SST_CollectedInformations | ForEach-Object {
                             Start-Sleep -Seconds 0.5 
                             $TD_SQLCommand.CommandText="INSERT INTO [dbo].[SANBase] (SANName,SANStatus,SANCodeLevel,SANCodeLevelLV,SANBrocadeProdName,SANMTM,SANSerialNumber,SANCustomerNumber,TimeStamp) VALUES ('$($_.'Swicht Name')','$($_.'Switch State')','$($_.'Fabric OS')','$($_.'Fabric OSLV')','$($_.'Brocade Product Name')','$($_.'MTM')','$($_.'Serial Num')','$CustomerNumber','$TimeStamp');"    
-                            Write-Host $TD_SQLCommand.ExecuteNonQuery() 
+                            Write-Debug $TD_SQLCommand.ExecuteNonQuery() 
                         }
                     }
-                Default {SST_ToolMessageCollector -TD_ToolMSGCollector "Something went wrong during saving the $SST_InfoType data in the local db." -TD_ToolMSGType Error -TD_Shown yes}
+                Default {SST_ToolMessageCollector -TD_ToolMSGCollector $("Something went wrong during saving the $SST_InfoType data in the local db.") -TD_ToolMSGType Error -TD_Shown yes}
             }
         }catch{
             Write-Host $_.exception.message
