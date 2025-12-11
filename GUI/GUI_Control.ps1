1a441de9273f215a1b0dab186ad36f7f2a8de264
ToolDoc <mailtodoc@web.de>
2025-12-11 11:49:22 +0100

    Update GUI_Control.ps1

--- GUI/GUI_Control.ps1
+++ GUI/GUI_Control.ps1
@@ -20,21 +20,38 @@ function Storage_SAN_Tool {
         [ValidateSet("DEFAULT","SAN","CONFIG","HEALTH","STORAGE","POWER","JobMode")]
         $CockpitView = $null
     )
+# ------------------------------
+# Load WPF styles BEFORE window
+# ------------------------------
+$StyleFiles = @(
+    "$PSRootPath\Resources\Styles\ColorStyle.xaml",
+    "$PSRootPath\Resources\Styles\AppStyle.xaml",
+    "$PSRootPath\Resources\Styles\TextBoxStyle.xaml",
+    "$PSRootPath\Resources\Styles\ButtonStyle.xaml"
+)
+
+$global:LoadedStyles = @()
+foreach ($file in $styleFiles) {
+    $dict = [Windows.Markup.XamlReader]::Parse((Get-Content $file -Raw))
+    $global:LoadedStyles += ,$dict
+}
+
 #$ErrorActionPreference="SilentlyContinue"
 SST_ToolMessageCollector -TD_ToolMSGCollector "Start reading Storage_SAN_Tool func" -TD_ToolMSGType Message -TD_Shown no
 $inputXAML=Get-Content -Raw -Path "$PSScriptRoot\MainWindow.xaml"
 [xml]$MainXAML=$inputXAML -replace 'mc:Ignorable="d"','' -replace "x:N","N" -replace "^<Win.*","<Window"
 [System.Xml.XmlNodeReader] $Mainreader = $MainXAML
 $MainWindow =[Windows.Markup.XamlReader]::Load($Mainreader)
-
+foreach ($style in $global:LoadedStyles) {
+    $MainWindow.Resources.MergedDictionaries.Add($style)
+}
 $MainXAML.SelectNodes("//*[@Name]") | ForEach-Object {Set-Variable -Name "TD_$($_.Name)" -Value $MainWindow.FindName($_.Name)}
-<# add ResourceDictionary for WPF to App #>
-$ColorStyles = [Windows.Markup.XamlReader]::Parse((Get-Content -Path "$PSRootPath\Resources\Styles\ColorStyle.xaml" -Raw))
-$AppStyles = [Windows.Markup.XamlReader]::Parse((Get-Content -Path "$PSRootPath\Resources\Styles\AppStyle.xaml" -Raw))
-$TextBoxStyles = [Windows.Markup.XamlReader]::Parse((Get-Content -Path "$PSRootPath\Resources\Styles\TextBoxStyle.xaml" -Raw))
-$ButtonStyles = [Windows.Markup.XamlReader]::Parse((Get-Content -Path "$PSRootPath\Resources\Styles\ButtonStyle.xaml" -Raw))
 
-foreach ($style in $ColorStyles,$AppStyles,$TextBoxStyles,$ButtonStyles){
+<# add ResourceDictionary for WPF to App #>
+
+#
+foreach ($file in $StyleFiles){
+    $style = [Windows.Markup.XamlReader]::Parse((Get-Content $file -Raw))
     $MainWindow.Resources.MergedDictionaries.Add( $style)
 }
 SST_ToolMessageCollector -TD_ToolMSGCollector "Load MainWindo Resources done" -TD_ToolMSGType Message -TD_Shown no
@@ -56,65 +73,42 @@ $DashBoardIcons.RefrehIcon96 = "$PSRootPath\Resources\Icons\iconrefresh96.png"
 <# Create UserControls as basis of Content for MainWindow #>
 $UserCxamlFile = Get-ChildItem "$PSScriptRoot\UserControl*.xaml"
 foreach($file in $UserCxamlFile){
-    $fileName = ($file.Name).trim(".xaml")
+    $fileName = ($file.Name).TrimEnd(".xaml")
     Set-Variable -Name "TD_$($fileName)"
-    <# if you want more UserControls then add them here #>
-    switch -wildcard ($fileName) {
-        "*l1" { 
-            $UserC1=Get-Content -Path $file -raw
-            $UserC1=$UserC1 -replace 'mc:Ignorable="d"','' -replace "x:N","N" -replace "^<Win.*","<Window"
-            [xml]$UserXAML1=$UserC1
-            $Userreader = New-Object System.Xml.XmlNodeReader $UserXAML1
-            $TD_UserControl1=[Windows.Markup.XamlReader]::Load($Userreader)
-            $UserXAML1.SelectNodes("//*[@Name]") | ForEach-Object {Set-Variable -Name "TD_$($_.Name)" -Value $TD_UserControl1.FindName($_.Name) }
-            $TD_UserControl1.DataContext = $DashBoardIcons
-         }
-        "*l2" { 
-            $UserC2=Get-Content -Path $file -raw
-            $UserC2=$UserC2 -replace 'mc:Ignorable="d"','' -replace "x:N","N" -replace "^<Win.*","<Window"
-            [xml]$UserXAML2=$UserC2
-            $Userreader = New-Object System.Xml.XmlNodeReader $UserXAML2
-            $TD_UserControl2=[Windows.Markup.XamlReader]::Load($Userreader)
-            $UserXAML2.SelectNodes("//*[@Name]") | ForEach-Object {Set-Variable -Name "TD_$($_.Name)" -Value $TD_UserControl2.FindName($_.Name) }
-         }
-        "*l3" { 
-            $UserC3=Get-Content -Path $file -raw
-            $UserC3=$UserC3 -replace 'mc:Ignorable="d"','' -replace "x:N","N" -replace "^<Win.*","<Window"
-            [xml]$UserXAML3=$UserC3
-            $Userreader = New-Object System.Xml.XmlNodeReader $UserXAML3
-            $TD_UserControl3=[Windows.Markup.XamlReader]::Load($Userreader)
-            $UserXAML3.SelectNodes("//*[@Name]") | ForEach-Object {Set-Variable -Name "TD_$($_.Name)" -Value $TD_UserControl3.FindName($_.Name) }
-         }
-        "*l4" { 
-            $UserC4=Get-Content -Path $file -raw
-            $UserC4=$UserC4 -replace 'mc:Ignorable="d"','' -replace "x:N","N" -replace "^<Win.*","<Window"
-            [xml]$UserXAML4=$UserC4
-            $Userreader = New-Object System.Xml.XmlNodeReader $UserXAML4
-            $TD_UserControl4=[Windows.Markup.XamlReader]::Load($Userreader)
-            $UserXAML4.SelectNodes("//*[@Name]") | ForEach-Object {Set-Variable -Name "TD_$($_.Name)" -Value $TD_UserControl4.FindName($_.Name) }
-            $TD_UserControl4.DataContext = $DashBoardIcons
-         }
-         "*l5" { 
-            $UserC5=Get-Content -Path $file -raw
-            $UserC5=$UserC5 -replace 'mc:Ignorable="d"','' -replace "x:N","N" -replace "^<Win.*","<Window"
-            [xml]$UserXAML5=$UserC5
-            $Userreader = New-Object System.Xml.XmlNodeReader $UserXAML5
-            $TD_UserControl5=[Windows.Markup.XamlReader]::Load($Userreader)
-            $UserXAML5.SelectNodes("//*[@Name]") | ForEach-Object {Set-Variable -Name "TD_$($_.Name)" -Value $TD_UserControl5.FindName($_.Name) }
-         }
-         "*l6" { 
-            $UserC6=Get-Content -Path $file -raw
-            $UserC6=$UserC6 -replace 'mc:Ignorable="d"','' -replace "x:N","N" -replace "^<Win.*","<Window"
-            [xml]$UserXAML6=$UserC6
-            $Userreader = New-Object System.Xml.XmlNodeReader $UserXAML6
-            $TD_UserControl6=[Windows.Markup.XamlReader]::Load($Userreader)
-            $UserXAML6.SelectNodes("//*[@Name]") | ForEach-Object {Set-Variable -Name "TD_$($_.Name)" -Value $TD_UserControl6.FindName($_.Name) }
-         }
-        Default { SST_ToolMessageCollector -TD_ToolMSGCollector "Create UserControl $fileName had a problem" -TD_ToolMSGType Error -TD_Shown no
-        Write-Host "Something did not work, start the application in debug mod and/or check the log file." -ForegroundColor Red; Start-Sleep -Seconds 5; exit 
-        }
+
+    $UserC=Get-Content -Path $file -Raw
+    $UserC=$UserC -replace 'mc:Ignorable="d"','' -replace "x:N","N"
+    [xml]$UserXAML=$UserC
+    $UserReader = New-Object System.Xml.XmlNodeReader $UserXAML
+    $TD_UserControl = [Windows.Markup.XamlReader]::Load($UserReader)
+
+    # --------------------------
+    # Inject Styles into UserControl
+    # --------------------------
+    foreach ($style in $global:LoadedStyles) {
+        $TD_UserControl.Resources.MergedDictionaries.Add($style)
     }
+
+    # --------------------------
+    # Set variables for named elements
+    # --------------------------
+    $UserXAML.SelectNodes("//*[@Name]") | ForEach-Object {
+        Set-Variable -Name "TD_$($_.Name)" -Value $TD_UserControl.FindName($_.Name)
+    }
+
+    # --------------------------
+    # Set DataContext if needed
+    # --------------------------
+    if ($fileName -like "*l1" -or $fileName -like "*l4") {
+        $TD_UserControl.DataContext = $DashBoardIcons
+    }
+
+    # --------------------------
+    # Assign to global variable for later use
+    # --------------------------
+    Set-Variable -Name "TD_$fileName" -Value $TD_UserControl
 }
+
 $TD_BTN_Dashboard.add_click({
     $TD_LB_ExpPathMainWindow.Content ="Export Path: $($TD_tb_ExportPath.Text)"
     if(!($TD_UserControl1.IsLoaded)){$TD_UserContrArea.Children.Add($TD_UserControl1)}
