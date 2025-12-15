150662fc33a462072debd792ec6bb7c0b057f98d
ToolDoc <mailtodoc@web.de>
2025-12-15 15:38:45 +0100

    Update IBM_BaseStorageInfos.ps1

--- Storage/IBM/IBM_BaseStorageInfos.ps1
+++ Storage/IBM/IBM_BaseStorageInfos.ps1
@@ -33,6 +33,7 @@ function IBM_BaseStorageInfos {
                     $TD_BaseInformations = SST_SpectrumSystemAPI -Endpoint lsnode -Body $null -RESTFileName $RESTFileName
                 }else {
                     $TD_BaseInformations = plink $TD_Device_UserName@$TD_Device_DeviceIP -pw $TD_Device_PW -batch 'lsnode -delim : && lsnode -nohdr |while read id name IO_group_id;do lsnode -delim : $id ;echo;done && lssystem -delim , |grep name'
+                    $TD_BaseInformations = $TD_BaseInformations |Select-Object -Skip 1
                 }
              }
             Default {
@@ -40,65 +41,110 @@ function IBM_BaseStorageInfos {
                     $TD_BaseInformations = SST_SpectrumSystemAPI -Endpoint lsnodecanister -Body $null -RESTFileName $RESTFileName
                 }else {
                     $TD_BaseInformations = plink $TD_Device_UserName@$TD_Device_DeviceIP -pw $TD_Device_PW -batch 'lsnodecanister -delim : && lsnodecanister -nohdr |while read id name IO_group_id;do lsnodecanister -delim : $id ;echo;done && lssystem -delim , |grep name'
+                    $TD_BaseInformations = $TD_BaseInformations |Select-Object -Skip 1
                 }
             }
-            
         }
-        $TD_BaseInformations = $TD_BaseInformations |Select-Object -Skip 1
-
     }
 
     process {
 
-        $TD_SystemInfo = IBM_SystemInfo -TD_Line_ID $TD_Line_ID -TD_Device_ConnectionTyp $TD_Device_ConnectionTyp -TD_Device_UserName $TD_Device_UserName -TD_Device_DeviceIP $TD_Device_DeviceIP -TD_Device_PW $TD_Device_PW
-
-
-        $TD_StorageInfo = foreach($TD_FSBaseInfo in $TD_BaseInformations){
-            $TD_FSBaseTemp = "" | Select-Object ID,Name,ClusterName,WWNN,Status,IO_group_id,IO_group_Name,Serial_Number,Code_Level,Config_Node,SideID,SideName,Prod_MTM,RecommendedPTF,MDiskTotalCapacity,MDiskFreeCapacity,MDiskUsedCapacity,PhysicalTotalCapacity,PhysicalFreeCapacity,HostUnmap,BackendUnmap
-            $TD_FSBaseTemp.ID = ($TD_FSBaseInfo|Select-String -Pattern '^(\d+):' -AllMatches).Matches.Groups[1].Value
-            $TD_FSBaseTemp.Name = ($TD_FSBaseInfo|Select-String -Pattern '^\d+:([a-zA-Z0-9-_]+):' -AllMatches).Matches.Groups[1].Value
-            $TD_FSBaseTemp.ClusterName = ($TD_BaseInformations|Select-String -Pattern '^name\,([\w\-]+)' -AllMatches).Matches.Groups[1].Value
-            $TD_FSBaseTemp.WWNN = ($TD_FSBaseInfo|Select-String -Pattern '^\d+:[a-zA-Z0-9-_]+:.*:([a-zA-Z0-9]+):(online|offline|service|flushing|adding|deleting|service):(\d+)' -AllMatches).Matches.Groups[1].Value
-            $TD_FSBaseTemp.Status = ($TD_FSBaseInfo|Select-String -Pattern '^\d+:[a-zA-Z0-9-_]+:.*:([a-zA-Z0-9-_]+):(online|offline|service|flushing|adding|deleting|service):(\d+)' -AllMatches).Matches.Groups[2].Value
-            $TD_FSBaseTemp.IO_group_id = ($TD_FSBaseInfo|Select-String -Pattern '^\d+:[a-zA-Z0-9-_]+:.*:([a-zA-Z0-9-_]+):(online|offline|service|flushing|adding|deleting|service):(\d+)' -AllMatches).Matches.Groups[3].Value
-            $TD_FSBaseTemp.IO_group_Name = ($TD_FSBaseInfo|Select-String -Pattern '\d+:([a-zA-Z0-9-_]+):(yes|no):' -AllMatches).Matches.Groups[1].Value
-            $TD_FSBaseBackEndSerial_Number = ($TD_FSBaseInfo|Select-String -Pattern ':\d+:\d+:([a-zA-Z0-9]+):' -AllMatches).Matches.Groups[1].Value
-            $TD_FSBaseSVCSerialNumber = ($TD_FSBaseInfo|Select-String -Pattern ':(\w{6,8}):(|\d+):(|\d+):(|\w{6,8}):' -AllMatches).Matches.Groups[1].Value
+        if($TD_Device_ConnectionTyp -eq "REST"){
             if($TD_Storage -eq "SVC"){
-               $TD_FSBaseTemp.Serial_Number = $TD_FSBaseSVCSerialNumber
+                $STONodeInfo = SST_SpectrumSystemAPI -Endpoint lsnode
             }else {
-                $TD_FSBaseTemp.Serial_Number = $TD_FSBaseBackEndSerial_Number
+                $STONodeInfo = SST_SpectrumSystemAPI -Endpoint lsnodecanister
             }
-            $TD_FSBaseTemp.Config_Node = ($TD_FSBaseInfo|Select-String -Pattern '\d+:([\w\-]+):(yes|no):' -AllMatches).Matches.Groups[2].Value
-            $TD_FSBaseTemp.SideID = ($TD_FSBaseInfo|Select-String -Pattern ':(\d+|):([a-zA-Z0-9-_]+)$' -AllMatches).Matches.Groups[1].Value
-            $TD_FSBaseTemp.SideName = ($TD_FSBaseInfo|Select-String -Pattern ':(\d+|):([a-zA-Z0-9-_]+)$' -AllMatches).Matches.Groups[2].Value
-            $TD_FSBaseTemp.Prod_MTM = ($TD_BaseInformations|Select-String -Pattern '^product_mtm:([a-zA-Z0-9-]+)' -AllMatches).Matches.Groups[1].Value
-            $TD_FSBaseTemp.Code_Level = ($TD_BaseInformations|Select-String -Pattern '^code_level:(\d+.\d+.\d+.\d+)' -AllMatches).Matches.Groups[1].Value
-
-            if ((![string]::IsNullOrEmpty($TD_FSBaseTemp.Prod_MTM))-and(![string]::IsNullOrEmpty($TD_FSBaseTemp.Code_Level))){
-                if(($TD_FSBaseTemp.Code_Level)-ne($TD_FSBaseTempCode_Level)){
-                    $TD_SpectrVirtuFWInfos = IBM_StorageSWCheck -IBM_CurrentSpectrVirtuFW $TD_FSBaseTemp.Code_Level -IBM_ProdMTM $TD_FSBaseTemp.Prod_MTM
-                    $TD_FSBaseTempCode_Level = $TD_FSBaseTemp.Code_Level
-                    
-                    Write-Debug -Message $TD_FSBaseTemp.Code_Level $TD_SpectrVirtuFWInfos
-
-                    [string]$TD_FSBaseTemp.RecommendedPTF = $TD_SpectrVirtuFWInfos.RecommendedPTF
-                }else {
-                    [string]$TD_FSBaseTemp.RecommendedPTF = $TD_SpectrVirtuFWInfos.RecommendedPTF
+            $STOSystemInfo = SST_SpectrumSystemAPI -Endpoint lssystem
+            [int]$imax = $STONodeInfo.Count
+            $TD_StorageInfo = for ($i = 0; $i -le $imax; $i++) {
+                $TD_FSBaseTemp = "" | Select-Object ID,Name,ClusterName,WWNN,Status,IO_group_id,IO_group_Name,Serial_Number,Code_Level,Config_Node,SideID,SideName,Prod_MTM,RecommendedPTF,MDiskTotalCapacity,MDiskFreeCapacity,MDiskUsedCapacity,PhysicalTotalCapacity,PhysicalFreeCapacity,HostUnmap,BackendUnmap,Topology,Layer,QuorumMode
+                $TD_FSBaseTemp.ID   =   $STONodeInfo.id[$i]
+                $TD_FSBaseTemp.Name   =   $STONodeInfo.name[$i]
+                $TD_FSBaseTemp.ClusterName   =   $STOSystemInfo.name
+                $TD_FSBaseTemp.WWNN   =   $STONodeInfo.WWNN[$i]
+                $TD_FSBaseTemp.Status   =   $STONodeInfo.status[$i]
+                $TD_FSBaseTemp.IO_group_id   =   $STONodeInfo.IO_group_id[$i]
+                $TD_FSBaseTemp.IO_group_Name   =   $STONodeInfo.IO_group_name[$i]
+                $TD_FSBaseTemp.Serial_Number   =   $STONodeInfo.enclosure_serial_number[$i]
+                $TD_FSBaseTemp.Config_Node   =   $STONodeInfo.config_node[$i]
+                $TD_FSBaseTemp.SideID   =   $STONodeInfo.site_id[$i]
+                $TD_FSBaseTemp.SideName   =   $STONodeInfo.site_name[$i]
+                $TD_FSBaseTemp.Prod_MTM   =   $STOSystemInfo.product_name
+                $TD_FSBaseTemp.Code_Level   =   $STOSystemInfo.code_level
+                <#try to get a RecommendedPTF level#>
+                if ((![string]::IsNullOrEmpty($TD_FSBaseTemp.Prod_MTM))-and(![string]::IsNullOrEmpty($TD_FSBaseTemp.Code_Level))){
+                    if(($TD_FSBaseTemp.Code_Level)-ne($TD_FSBaseTempCode_Level)){
+                        $TD_SpectrVirtuFWInfos = IBM_StorageSWCheck -IBM_CurrentSpectrVirtuFW $TD_FSBaseTemp.Code_Level -IBM_ProdMTM $TD_FSBaseTemp.Prod_MTM
+                        $TD_FSBaseTempCode_Level = $TD_FSBaseTemp.Code_Level
+                        [string]$TD_FSBaseTemp.RecommendedPTF = $TD_SpectrVirtuFWInfos.RecommendedPTF
+                    }else {
+                        [string]$TD_FSBaseTemp.RecommendedPTF = $TD_SpectrVirtuFWInfos.RecommendedPTF
+                    }
                 }
+                $TD_FSBaseTemp.MDiskTotalCapacity   =   $STOSystemInfo.total_mdisk_capacity
+                $TD_FSBaseTemp.MDiskFreeCapacity   =   $STOSystemInfo.total_free_space
+                $TD_FSBaseTemp.MDiskUsedCapacity   =   $STOSystemInfo.total_used_capacity
+                $TD_FSBaseTemp.PhysicalTotalCapacity   =   $STOSystemInfo.physical_capacity
+                $TD_FSBaseTemp.PhysicalFreeCapacity   =   $STOSystemInfo.physical_free_capacity
+                $TD_FSBaseTemp.HostUnmap   =   $STOSystemInfo.host_unmap
+                $TD_FSBaseTemp.BackendUnmap   =   $STOSystemInfo.backend_unmap
+                $TD_FSBaseTemp.Topology   =   $STOSystemInfo.topology
+                $TD_FSBaseTemp.Layer   =   $STOSystemInfo.layer
+                $TD_FSBaseTemp.QuorumMode   =   $STOSystemInfo.quorum_mode
+                $TD_FSBaseTemp
+                $ProgCounter++
+                Write-ProgressBar -ProgressBar $ProgressBar -Activity "Collect data for Device $($TD_Line_ID) $($STONodeInfo.name[$i])" -PercentComplete (($ProgCounter/$TD_BaseInformations.Count) * 100)
             }
-            $TD_FSBaseTemp.MDiskTotalCapacity=$TD_SystemInfo.'MDiskTotalCapacity'
-            $TD_FSBaseTemp.MDiskFreeCapacity=$TD_SystemInfo.'MDiskFreeCapacity'
-            $TD_FSBaseTemp.MDiskUsedCapacity=$TD_SystemInfo.'MDiskUsedCapacity'
-            $TD_FSBaseTemp.PhysicalTotalCapacity=$TD_SystemInfo.'PhysicalTotalCapacity'
-            $TD_FSBaseTemp.PhysicalFreeCapacity=$TD_SystemInfo.'PhysicalFreeCapacity'
-            $TD_FSBaseTemp.HostUnmap=$TD_SystemInfo.'HostUnmap'
-            $TD_FSBaseTemp.BackendUnmap=$TD_SystemInfo.'BackendUnmap'
-            if(([String]::IsNullOrEmpty($TD_FSBaseTemp.ID))){continue}
-            $TD_FSBaseTemp
-            <# Progressbar  #>
-            $ProgCounter++
-            Write-ProgressBar -ProgressBar $ProgressBar -Activity "Collect data for Device $($TD_Line_ID) $($TD_Device_DeviceName)" -PercentComplete (($ProgCounter/$TD_BaseInformations.Count) * 100)
+        }else{
+            $TD_SystemInfo = IBM_SystemInfo -TD_Line_ID $TD_Line_ID -TD_Device_ConnectionTyp $TD_Device_ConnectionTyp -TD_Device_UserName $TD_Device_UserName -TD_Device_DeviceIP $TD_Device_DeviceIP -TD_Device_PW $TD_Device_PW
+            $TD_StorageInfo = foreach($TD_FSBaseInfo in $TD_BaseInformations){
+                $TD_FSBaseTemp = "" | Select-Object ID,Name,ClusterName,WWNN,Status,IO_group_id,IO_group_Name,Serial_Number,Code_Level,Config_Node,SideID,SideName,Prod_MTM,RecommendedPTF,MDiskTotalCapacity,MDiskFreeCapacity,MDiskUsedCapacity,PhysicalTotalCapacity,PhysicalFreeCapacity,HostUnmap,BackendUnmap,Topology,Layer,QuorumMode
+                $TD_FSBaseTemp.ID = ($TD_FSBaseInfo|Select-String -Pattern '^(\d+):' -AllMatches).Matches.Groups[1].Value
+                $TD_FSBaseTemp.Name = ($TD_FSBaseInfo|Select-String -Pattern '^\d+:([a-zA-Z0-9-_]+):' -AllMatches).Matches.Groups[1].Value
+                $TD_FSBaseTemp.ClusterName = ($TD_BaseInformations|Select-String -Pattern '^name\,([\w\-]+)' -AllMatches).Matches.Groups[1].Value
+                $TD_FSBaseTemp.WWNN = ($TD_FSBaseInfo|Select-String -Pattern '^\d+:[a-zA-Z0-9-_]+:.*:([a-zA-Z0-9]+):(online|offline|service|flushing|adding|deleting|service):(\d+)' -AllMatches).Matches.Groups[1].Value
+                $TD_FSBaseTemp.Status = ($TD_FSBaseInfo|Select-String -Pattern '^\d+:[a-zA-Z0-9-_]+:.*:([a-zA-Z0-9-_]+):(online|offline|service|flushing|adding|deleting|service):(\d+)' -AllMatches).Matches.Groups[2].Value
+                $TD_FSBaseTemp.IO_group_id = ($TD_FSBaseInfo|Select-String -Pattern '^\d+:[a-zA-Z0-9-_]+:.*:([a-zA-Z0-9-_]+):(online|offline|service|flushing|adding|deleting|service):(\d+)' -AllMatches).Matches.Groups[3].Value
+                $TD_FSBaseTemp.IO_group_Name = ($TD_FSBaseInfo|Select-String -Pattern '\d+:([a-zA-Z0-9-_]+):(yes|no):' -AllMatches).Matches.Groups[1].Value
+                $TD_FSBaseBackEndSerial_Number = ($TD_FSBaseInfo|Select-String -Pattern ':\d+:\d+:([a-zA-Z0-9]+):' -AllMatches).Matches.Groups[1].Value
+                $TD_FSBaseSVCSerialNumber = ($TD_FSBaseInfo|Select-String -Pattern ':(\w{6,8}):(|\d+):(|\d+):(|\w{6,8}):' -AllMatches).Matches.Groups[1].Value
+                if($TD_Storage -eq "SVC"){
+                   $TD_FSBaseTemp.Serial_Number = $TD_FSBaseSVCSerialNumber
+                }else {
+                    $TD_FSBaseTemp.Serial_Number = $TD_FSBaseBackEndSerial_Number
+                }
+                $TD_FSBaseTemp.Config_Node = ($TD_FSBaseInfo|Select-String -Pattern '\d+:([\w\-]+):(yes|no):' -AllMatches).Matches.Groups[2].Value
+                $TD_FSBaseTemp.SideID = ($TD_FSBaseInfo|Select-String -Pattern ':(\d+|):([a-zA-Z0-9-_]+)$' -AllMatches).Matches.Groups[1].Value
+                $TD_FSBaseTemp.SideName = ($TD_FSBaseInfo|Select-String -Pattern ':(\d+|):([a-zA-Z0-9-_]+)$' -AllMatches).Matches.Groups[2].Value
+                $TD_FSBaseTemp.Prod_MTM = ($TD_BaseInformations|Select-String -Pattern '^product_mtm:([a-zA-Z0-9-]+)' -AllMatches).Matches.Groups[1].Value
+                $TD_FSBaseTemp.Code_Level = ($TD_BaseInformations|Select-String -Pattern '^code_level:(\d+.\d+.\d+.\d+)' -AllMatches).Matches.Groups[1].Value
+
+                if ((![string]::IsNullOrEmpty($TD_FSBaseTemp.Prod_MTM))-and(![string]::IsNullOrEmpty($TD_FSBaseTemp.Code_Level))){
+                    if(($TD_FSBaseTemp.Code_Level)-ne($TD_FSBaseTempCode_Level)){
+                        $TD_SpectrVirtuFWInfos = IBM_StorageSWCheck -IBM_CurrentSpectrVirtuFW $TD_FSBaseTemp.Code_Level -IBM_ProdMTM $TD_FSBaseTemp.Prod_MTM
+                        $TD_FSBaseTempCode_Level = $TD_FSBaseTemp.Code_Level
+
+                        Write-Debug -Message $TD_FSBaseTemp.Code_Level $TD_SpectrVirtuFWInfos
+
+                        [string]$TD_FSBaseTemp.RecommendedPTF = $TD_SpectrVirtuFWInfos.RecommendedPTF
+                    }else {
+                        [string]$TD_FSBaseTemp.RecommendedPTF = $TD_SpectrVirtuFWInfos.RecommendedPTF
+                    }
+                }
+                $TD_FSBaseTemp.MDiskTotalCapacity=$TD_SystemInfo.'MDiskTotalCapacity'
+                $TD_FSBaseTemp.MDiskFreeCapacity=$TD_SystemInfo.'MDiskFreeCapacity'
+                $TD_FSBaseTemp.MDiskUsedCapacity=$TD_SystemInfo.'MDiskUsedCapacity'
+                $TD_FSBaseTemp.PhysicalTotalCapacity=$TD_SystemInfo.'PhysicalTotalCapacity'
+                $TD_FSBaseTemp.PhysicalFreeCapacity=$TD_SystemInfo.'PhysicalFreeCapacity'
+                $TD_FSBaseTemp.HostUnmap=$TD_SystemInfo.'HostUnmap'
+                $TD_FSBaseTemp.BackendUnmap=$TD_SystemInfo.'BackendUnmap'
+                if(([String]::IsNullOrEmpty($TD_FSBaseTemp.ID))){continue}
+                $TD_FSBaseTemp
+                <# Progressbar  #>
+                $ProgCounter++
+                Write-ProgressBar -ProgressBar $ProgressBar -Activity "Collect data for Device $($TD_Line_ID) $($TD_Device_DeviceName)" -PercentComplete (($ProgCounter/$TD_BaseInformations.Count) * 100)
+            }
         }
         
     }
