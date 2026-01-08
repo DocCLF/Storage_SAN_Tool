1a6878b4762ace4d00e2b89227aed72c13cbbb0d
ToolDoc <mailtodoc@web.de>
2026-01-08 12:17:51 +0100

    Update PWSH7Classes.ps1

--- Resources/PWSHClasses/PWSH7Classes.ps1
+++ Resources/PWSHClasses/PWSH7Classes.ps1
@@ -28,41 +28,47 @@ public class RootViewModel : INotifyPropertyChanged
     public event PropertyChangedEventHandler PropertyChanged;
 }
 
+public enum DetailLevel
+{
+    Min = 0,
+    Full = 1
+}
+
 public class BaseStorageRow
 {
     public string ID { get; set; }
     public string Name { get; set; }
+    public string ClusterName { get; set; }
     public string WWNN { get; set; }
     public string Status { get; set; }
     public string IO_group_id { get; set; }
     public string IO_group_Name { get; set; }
+    public string Prod_MTM { get; set; }
     public string Serial_Number { get; set; }
     public string Code_Level { get; set; }
     public string RecommendedPTF { get; set; }
     public string Config_Node { get; set; }
     public string SideID { get; set; }
     public string SideName { get; set; }
+    public string MDiskTotalCapacity { get; set; }
+    public string MDiskFreeCapacity { get; set; }
+    public string MDiskUsedCapacity { get; set; }
+    public string PhysicalTotalCapacity { get; set; }
+    public string PhysicalFreeCapacity { get; set; }
+    public string HostUnmap { get; set; }
+    public string BackendUnmap { get; set; }
+    public string Topology { get; set; }
+    public string Layer { get; set; }
+    public string QuorumMode { get; set; }
 }
-public class BaseStorageRowMin
-{
-    public string ID { get; set; }
-    public string Name { get; set; }
-    public string WWNN { get; set; }
-    public string Status { get; set; }
-    public string IO_group_id { get; set; }
-    public string IO_group_Name { get; set; }
-    public string SideID { get; set; }
-    public string SideName { get; set; }
-}
+
 public class DeviceToggle : INotifyPropertyChanged
 {
     private bool _isChecked;
     public string Id { get; set; }
     public string Label { get; set; }
 
-    // jede Checkbox bekommt "ihre" Daten fürs Grid
-    public ObservableCollection<BaseStorageRow> Rows { get; } = new ObservableCollection<BaseStorageRow>();
-    public ObservableCollection<BaseStorageRowMin> RowsMin { get; } = new ObservableCollection<BaseStorageRowMin>();
+    public ObservableCollection<BaseStorageRow> DGRow { get; } = new ObservableCollection<BaseStorageRow>();
 
     public bool IsChecked
     {
