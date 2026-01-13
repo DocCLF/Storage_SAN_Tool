a5844b8168f4c14a07b8931cc79f5032cc912eaf
ToolDoc <mailtodoc@web.de>
2026-01-13 18:05:07 +0100

    Create IBM_RESTUserInfo.ps1

---
+++ Storage/IBM/REST/IBM_RESTUserInfo.ps1
@@ -0,0 +1,93 @@
+function IBM_RESTUserInfo {
+    [CmdletBinding()]
+    param (
+        [Int16]$TD_Line_ID,
+        [string]$TD_Device_ConnectionTyp,
+        [string]$TD_Device_UserName,
+        [string]$TD_Device_DeviceIP,
+        #[string]$TD_Device_DeviceName,
+        [string]$TD_Device_PW,
+        [string]$TD_Export = "yes",
+        [string]$TD_Exportpath,
+        $body = @{} <# This is the only part you are allowed to change. #>
+    )
+    
+    begin {
+        $ErrorActionPreference="SilentlyContinue"
+        [int]$ProgCounter=0
+        $ProgressBar = New-ProgressBar
+        $BaseUrl = "https://$TD_Device_DeviceIP"+":7443"
+        if(Test-Path -Path "$PSRootPath\Resources\DBFolder\ToolDB\ToolDB.db"){
+            $RESTInfo = SST_RESTDBControl -SST_InfoType "UseStorageToken" -SST_BaseUrl $BaseUrl
+            if(([string]::IsNullOrEmpty($RESTInfo)) -and ($TD_Device_ConnectionTyp -eq "REST")){
+                SST_GetSpectrumToken -TD_Device_UserName $TD_Device_UserName -TD_Device_DeviceIP $TD_Device_DeviceIP -TD_Device_PW $TD_Device_PW
+            }
+        }
+        if([string]::IsNullOrWhiteSpace($TD_Device_ConnectionTyp)){
+            $TD_Device_ConnectionTyp = SST_GetSpectrumToken -TD_Device_UserName $TD_Device_UserName -TD_Device_DeviceIP $TD_Device_DeviceIP -TD_Device_PW $TD_Device_PW 
+        }
+        Clear-Variable -Name TD_Device_PW -Force
+        if($TD_Device_ConnectionTyp -eq "REST"){
+            $TD_DeviceInformation = SST_SpectrumSystemAPI -Endpoint lsuser -Body $null -BaseUrl $BaseUrl -RESTInfo $RESTInfo
+            $STONodeInfo = SST_SpectrumSystemAPI -Endpoint lsnode -Body $null -BaseUrl $BaseUrl -RESTInfo $RESTInfo
+        }else {
+            <#switch to the ssh version and leave this func #>
+            return $null
+        }
+        [int]$imax = $STONodeInfo.Count
+        for ($i = 0; $i -le $imax; $i++) {
+            if($STONodeInfo.config_node[$i] -eq "yes"){
+                $IBMSTOWWNN = $STONodeInfo.WWNN[$i]
+                $IBMSTOSN = if($STONodeInfo.enclosure_serial_number[$i] -eq ""){$STONodeInfo.panel_name[$i]}else{$STONodeInfo.enclosure_serial_number[$i]}
+            }
+        }
+    }
+
+    process{
+        [int]$imax = $TD_DeviceInformation.Count
+        [array]$TD_UserInfoResault = for ($i = 0; $i -le $imax; $i++) {
+            <# Node Info#>
+            $TD_Userinfo = "" | Select-Object ID,User_Name,Password,SSH_Key,Remote,UserGrp_ID,UserGrp_Name,Owner_ID,Owner_Name,Locked,PW_Change_required,WWNN,SerialNumber
+
+            $TD_Userinfo.ID      = $TD_DeviceInformation.id[$i]
+            $TD_Userinfo.User_Name           = $TD_DeviceInformation.name[$i]
+            $TD_Userinfo.Password               = $TD_DeviceInformation.password[$i]
+            $TD_Userinfo.SSH_Key             = $TD_DeviceInformation.ssh_key[$i]
+            $TD_Userinfo.Remote     = $TD_DeviceInformation.remote[$i]
+            $TD_Userinfo.UserGrp_ID   = $TD_DeviceInformation.usergrp_id[$i]
+            $TD_Userinfo.UserGrp_Name           = $TD_DeviceInformation.usergrp_name[$i]
+            $TD_Userinfo.Owner_ID       = $TD_DeviceInformation.owner_id[$i]
+            $TD_Userinfo.Owner_Name         = $TD_DeviceInformation.owner_name[$i]
+            $TD_Userinfo.Locked           = $TD_DeviceInformation.locked[$i]
+            $TD_Userinfo.PW_Change_required         = $TD_DeviceInformation.password_change_required[$i]
+
+            $TD_Userinfo.WWNN = $IBMSTOWWNN
+            $TD_Userinfo.SerialNumber = $IBMSTOSN
+
+            $TD_Userinfo
+
+            <# Progressbar  #>
+            $ProgCounter++
+            Write-ProgressBar -ProgressBar $ProgressBar -Activity "Collect data for Device $($TD_Line_ID) $($TD_Device_DeviceName)" -PercentComplete (($ProgCounter/$imax) * 100)
+        }
+    }
+    
+    end {
+
+        Close-ProgressBar -ProgressBar $ProgressBar
+        if($TD_Export -eq "yes"){
+            
+            if([string]$TD_Exportpath -ne "$PSCommandPath\ToolLog\"){
+                $TD_UserInfoResault | Export-Csv -Path $TD_Exportpath\$($TD_Line_ID)_$($TD_Device_DeviceName)_User_Result_$(Get-Date -Format "yyyy-MM-dd").csv -NoTypeInformation
+                SST_ToolMessageCollector -TD_ToolMSGCollector "$TD_Exportpath\$($TD_Line_ID)_$($TD_Device_DeviceName)_User_Result_$(Get-Date -Format "yyyy-MM-dd").csv" -TD_ToolMSGType Debug
+            }else {
+                $TD_UserInfoResault | Export-Csv -Path $PSCommandPath\ToolLog\$($TD_Line_ID)_$($TD_Device_DeviceName)_User_Result_$(Get-Date -Format "yyyy-MM-dd").csv -NoTypeInformation
+                SST_ToolMessageCollector -TD_ToolMSGCollector "$PSCommandPath\ToolLog\$($TD_Line_ID)_$($TD_Device_DeviceName)_User_Result_$(Get-Date -Format "yyyy-MM-dd").csv" -TD_ToolMSGType Debug
+            }
+        }else {
+            <# output on the promt #>
+            return $TD_UserInfoResault
+        }
+        return $TD_UserInfoResault
+    }
+}
