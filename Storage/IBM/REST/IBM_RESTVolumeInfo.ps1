4e7c4fde044a11b0b12c0d5905be0c4f3a34b1cf
ToolDoc <mailtodoc@web.de>
2026-01-15 16:50:17 +0100

    Update IBM_RESTVolumeInfo.ps1

--- Storage/IBM/REST/IBM_RESTVolumeInfo.ps1
+++ Storage/IBM/REST/IBM_RESTVolumeInfo.ps1
@@ -45,60 +45,60 @@ function IBM_RESTVolumeInfo {
 
     process{
         [int]$imax = $TD_DeviceInformation.Count
-        [array]$TD_UserInfoResault = for ($i = 0; $i -le $imax; $i++) {
+        [array]$TD_VDiskFuncResault = for ($i = 0; $i -le $imax; $i++) {
             <# Node Info#>
-            $TD_Userinfo = "" | Select-Object ID,Name,IOGroupID,IOGroupName,Status,MdiskGrpID,MdiskGrpName,Capacity,Type,FCID,FCName,`
+            $TD_VDiskinfo = "" | Select-Object ID,Name,IOGroupID,IOGroupName,Status,MdiskGrpID,MdiskGrpName,Capacity,Type,FCID,FCName,`
                                             RCID,RCName,VdiskUID,FCMapCount,CopyCount,FastWriteState,SECopyCount,RCChange,CompressedCopyCount,ParentMdiskGrpID,ParentMdiskGrpName,`
                                             OwnerID,OwnerName,Formatting,Encrypt,VolumeID,VolumeName,Function,VolumeGroupID,VolumeGroupName,Protocol,isSnapshot,`
-                                            SnapshotCount,VolumeType,ReplicationMode,isSafeguardedSnapshot,SafeguardedSnapshotCount
+                                            SnapshotCount,VolumeType,ReplicationMode,isSafeguardedSnapshot,SafeguardedSnapshotCount,WWNN,SerialNumber
 
-            $TD_Userinfo.ID      = $TD_DeviceInformation.id[$i]
-            $TD_Userinfo.Name           = $TD_DeviceInformation.name[$i]
-            $TD_Userinfo.IOGroupID               = $TD_DeviceInformation.IO_group_id[$i]
-            $TD_Userinfo.IOGroupName             = $TD_DeviceInformation.IO_group_name[$i]
-            $TD_Userinfo.Status     = $TD_DeviceInformation.status[$i]
-            $TD_Userinfo.MdiskGrpID   = $TD_DeviceInformation.mdisk_grp_id[$i]
-            $TD_Userinfo.MdiskGrpName           = $TD_DeviceInformation.mdisk_grp_name[$i]
-            $TD_Userinfo.Capacity       = $TD_DeviceInformation.capacity[$i]
-            $TD_Userinfo.Type         = $TD_DeviceInformation.type[$i]
-            $TD_Userinfo.FCID           = $TD_DeviceInformation.FC_id[$i]
-            $TD_Userinfo.FCName         = $TD_DeviceInformation.FC_name[$i]
+            $TD_VDiskinfo.ID                         = $TD_DeviceInformation.id[$i]
+            $TD_VDiskinfo.Name                       = $TD_DeviceInformation.name[$i]
+            $TD_VDiskinfo.IOGroupID                  = $TD_DeviceInformation.IO_group_id[$i]
+            $TD_VDiskinfo.IOGroupName                = $TD_DeviceInformation.IO_group_name[$i]
+            $TD_VDiskinfo.Status                     = $TD_DeviceInformation.status[$i]
+            $TD_VDiskinfo.MdiskGrpID                 = $TD_DeviceInformation.mdisk_grp_id[$i]
+            $TD_VDiskinfo.MdiskGrpName               = $TD_DeviceInformation.mdisk_grp_name[$i]
+            $TD_VDiskinfo.Capacity                   = $TD_DeviceInformation.capacity[$i]
+            $TD_VDiskinfo.Type                       = $TD_DeviceInformation.type[$i]
+            $TD_VDiskinfo.FCID                       = $TD_DeviceInformation.FC_id[$i]
+            $TD_VDiskinfo.FCName                     = $TD_DeviceInformation.FC_name[$i]
             #   RCID,RCName,VdiskUID,FCMapCount,CopyCount,FastWriteState,SECopyCount,RCChange,CompressedCopyCount,ParentMdiskGrpID,ParentMdiskGrpName,`
-            $TD_Userinfo.RCID      = $TD_DeviceInformation.RC_id[$i]
-            $TD_Userinfo.RCName           = $TD_DeviceInformation.RC_name[$i]
-            $TD_Userinfo.VdiskUID               = $TD_DeviceInformation.vdisk_UID[$i]
-            $TD_Userinfo.FCMapCount             = $TD_DeviceInformation.fc_map_count[$i]
-            $TD_Userinfo.CopyCount     = $TD_DeviceInformation.copy_count[$i]
-            $TD_Userinfo.FastWriteState   = $TD_DeviceInformation.fast_write_state[$i]
-            $TD_Userinfo.SECopyCount           = $TD_DeviceInformation.se_copy_count[$i]
-            $TD_Userinfo.RCChange       = $TD_DeviceInformation.RC_change[$i]
-            $TD_Userinfo.CompressedCopyCount         = $TD_DeviceInformation.compressed_copy_count[$i]
-            $TD_Userinfo.ParentMdiskGrpID           = $TD_DeviceInformation.parent_mdisk_grp_id[$i]
-            $TD_Userinfo.ParentMdiskGrpName         = $TD_DeviceInformation.parent_mdisk_grp_name[$i]
+            $TD_VDiskinfo.RCID                       = $TD_DeviceInformation.RC_id[$i]
+            $TD_VDiskinfo.RCName                     = $TD_DeviceInformation.RC_name[$i]
+            $TD_VDiskinfo.VdiskUID                   = $TD_DeviceInformation.vdisk_UID[$i]
+            $TD_VDiskinfo.FCMapCount                 = $TD_DeviceInformation.fc_map_count[$i]
+            $TD_VDiskinfo.CopyCount                  = $TD_DeviceInformation.copy_count[$i]
+            $TD_VDiskinfo.FastWriteState             = $TD_DeviceInformation.fast_write_state[$i]
+            $TD_VDiskinfo.SECopyCount                = $TD_DeviceInformation.se_copy_count[$i]
+            $TD_VDiskinfo.RCChange                   = $TD_DeviceInformation.RC_change[$i]
+            $TD_VDiskinfo.CompressedCopyCount        = $TD_DeviceInformation.compressed_copy_count[$i]
+            $TD_VDiskinfo.ParentMdiskGrpID           = $TD_DeviceInformation.parent_mdisk_grp_id[$i]
+            $TD_VDiskinfo.ParentMdiskGrpName         = $TD_DeviceInformation.parent_mdisk_grp_name[$i]
             #   OwnerID,OwnerName,Formatting,Encrypt,VolumeID,VolumeName,Function,VolumeGroupID,VolumeGroupName,Protocol,isSnapshot,`
-            $TD_Userinfo.OwnerID      = $TD_DeviceInformation.owner_id[$i]
-            $TD_Userinfo.OwnerName           = $TD_DeviceInformation.owner_name[$i]
-            $TD_Userinfo.Formatting               = $TD_DeviceInformation.formatting[$i]
-            $TD_Userinfo.Encrypt             = $TD_DeviceInformation.encrypt[$i]
-            $TD_Userinfo.VolumeID     = $TD_DeviceInformation.volume_id[$i]
-            $TD_Userinfo.VolumeName   = $TD_DeviceInformation.volume_name[$i]
-            $TD_Userinfo.Function           = $TD_DeviceInformation.function[$i]
-            $TD_Userinfo.VolumeGroupID       = $TD_DeviceInformation.volume_group_id[$i]
-            $TD_Userinfo.VolumeGroupName         = $TD_DeviceInformation.volume_group_name[$i]
-            $TD_Userinfo.Protocol           = $TD_DeviceInformation.protocol[$i]
-            $TD_Userinfo.isSnapshot         = $TD_DeviceInformation.is_snapshot[$i]
+            $TD_VDiskinfo.OwnerID                    = $TD_DeviceInformation.owner_id[$i]
+            $TD_VDiskinfo.OwnerName                  = $TD_DeviceInformation.owner_name[$i]
+            $TD_VDiskinfo.Formatting                 = $TD_DeviceInformation.formatting[$i]
+            $TD_VDiskinfo.Encrypt                    = $TD_DeviceInformation.encrypt[$i]
+            $TD_VDiskinfo.VolumeID                   = $TD_DeviceInformation.volume_id[$i]
+            $TD_VDiskinfo.VolumeName                 = $TD_DeviceInformation.volume_name[$i]
+            $TD_VDiskinfo.Function                   = $TD_DeviceInformation.function[$i]
+            $TD_VDiskinfo.VolumeGroupID              = $TD_DeviceInformation.volume_group_id[$i]
+            $TD_VDiskinfo.VolumeGroupName            = $TD_DeviceInformation.volume_group_name[$i]
+            $TD_VDiskinfo.Protocol                   = $TD_DeviceInformation.protocol[$i]
+            $TD_VDiskinfo.isSnapshot                 = $TD_DeviceInformation.is_snapshot[$i]
             #   SnapshotCount,VolumeType,ReplicationMode,isSafeguardedSnapshot,SafeguardedSnapshotCount
-            $TD_Userinfo.SnapshotCount           = $TD_DeviceInformation.snapshot_count[$i]
-            $TD_Userinfo.VolumeType       = $TD_DeviceInformation.volume_type[$i]
-            $TD_Userinfo.ReplicationMode         = $TD_DeviceInformation.replication_mode[$i]
-            $TD_Userinfo.isSafeguardedSnapshot           = $TD_DeviceInformation.is_safeguarded_snapshot[$i]
-            $TD_Userinfo.SafeguardedSnapshotCount         = $TD_DeviceInformation.safeguarded_snapshot_count[$i]
+            $TD_VDiskinfo.SnapshotCount              = $TD_DeviceInformation.snapshot_count[$i]
+            $TD_VDiskinfo.VolumeType                 = $TD_DeviceInformation.volume_type[$i]
+            $TD_VDiskinfo.ReplicationMode            = $TD_DeviceInformation.replication_mode[$i]
+            $TD_VDiskinfo.isSafeguardedSnapshot      = $TD_DeviceInformation.is_safeguarded_snapshot[$i]
+            $TD_VDiskinfo.SafeguardedSnapshotCount   = $TD_DeviceInformation.safeguarded_snapshot_count[$i]
 
 
-            $TD_Userinfo.WWNN = $IBMSTOWWNN
-            $TD_Userinfo.SerialNumber = $IBMSTOSN
+            $TD_VDiskinfo.WWNN           = $IBMSTOWWNN
+            $TD_VDiskinfo.SerialNumber   = $IBMSTOSN
 
-            $TD_Userinfo
+            $TD_VDiskinfo
 
             <# Progressbar  #>
             $ProgCounter++
@@ -112,16 +112,16 @@ function IBM_RESTVolumeInfo {
         if($TD_Export -eq "yes"){
 
             if([string]$TD_Exportpath -ne "$PSCommandPath\ToolLog\"){
-                $TD_UserInfoResault | Export-Csv -Path $TD_Exportpath\$($TD_Line_ID)_$($TD_Device_DeviceName)_Volume_Result_$(Get-Date -Format "yyyy-MM-dd").csv -NoTypeInformation
+                $TD_VDiskFuncResault | Export-Csv -Path $TD_Exportpath\$($TD_Line_ID)_$($TD_Device_DeviceName)_Volume_Result_$(Get-Date -Format "yyyy-MM-dd").csv -NoTypeInformation
                 SST_ToolMessageCollector -TD_ToolMSGCollector "$TD_Exportpath\$($TD_Line_ID)_$($TD_Device_DeviceName)_Volume_Result_$(Get-Date -Format "yyyy-MM-dd").csv" -TD_ToolMSGType Debug
             }else {
-                $TD_UserInfoResault | Export-Csv -Path $PSCommandPath\ToolLog\$($TD_Line_ID)_$($TD_Device_DeviceName)_Volume_Result_$(Get-Date -Format "yyyy-MM-dd").csv -NoTypeInformation
+                $TD_VDiskFuncResault | Export-Csv -Path $PSCommandPath\ToolLog\$($TD_Line_ID)_$($TD_Device_DeviceName)_Volume_Result_$(Get-Date -Format "yyyy-MM-dd").csv -NoTypeInformation
                 SST_ToolMessageCollector -TD_ToolMSGCollector "$PSCommandPath\ToolLog\$($TD_Line_ID)_$($TD_Device_DeviceName)_Volume_Result_$(Get-Date -Format "yyyy-MM-dd").csv" -TD_ToolMSGType Debug
             }
         }else {
             <# output on the promt #>
-            return $TD_UserInfoResault
+            return $TD_VDiskFuncResault
         }
-        return $TD_UserInfoResault
+        return $TD_VDiskFuncResault
     }
 }
