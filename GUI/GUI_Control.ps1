b45407d9ec89f60460f471a4ada9bc466063ecb5
ToolDoc <mailtodoc@web.de>
2026-01-15 16:49:16 +0100

    Update GUI_Control.ps1

--- GUI/GUI_Control.ps1
+++ GUI/GUI_Control.ps1
@@ -350,10 +350,10 @@ $TD_BTN_IBM_BaseStorageInfo.add_click({
             IO_group_id    = 'IO_group_id'
             IO_group_Name  = 'IO_group_Name'
             Prod_MTM       = 'Prod_MTM'
-            Serial_Number  = 'Serial_Number'
-            Code_Level     = 'Code_Level'
+            Serial_Number  = 'SerialNumber'
+            Code_Level     = 'CodeLevel'
             RecommendedPTF = 'RecommendedPTF'
-            Config_Node    = 'Config_Node'
+            Config_Node    = 'ConfigNode'
             SideID         = 'SideID'
             SideName       = 'SideName'
         } 
