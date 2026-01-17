b0358e02df6bdff692c7862181e09ced92e6a1c4
ToolDoc <mailtodoc@web.de>
2026-01-17 20:49:15 +0100

    Update SST_HelperFuncs.ps1

--- TOOLFunc/GUIBasics/SST_HelperFuncs.ps1
+++ TOOLFunc/GUIBasics/SST_HelperFuncs.ps1
@@ -42,24 +42,75 @@ function New-DeviceBlock {
         [object]$SSHFunc
     )
 
-    # 1) Daten holen (REST -> wenn leer -> SSH)
+    [bool]$FallbacktoSSH = $false
+    # 1) Daten holen (REST -> wenn leer -> SSH) StorageInfo
     $pw = [Net.NetworkCredential]::new('', $Device.Password).Password
     
     $FunResult = & $RESTFunc -TD_Line_ID $Device.ID -TD_Device_UserName $Device.UserName -TD_Device_DeviceIP $Device.IPAddress -TD_Device_PW $pw -TD_Exportpath $ExportPath
-    if (([string]::IsNullOrWhiteSpace($FunResult.StorageInfo.ID))-or(!($FunResult))) {
-        $FunResult = & $SSHFunc -TD_Line_ID $Device.ID -TD_Device_ConnectionTyp $Device.ConnectionTyp -TD_Device_UserName $Device.UserName -TD_Device_DeviceIP $Device.IPAddress -TD_Device_PW $pw -TD_Exportpath $ExportPath
+
+    $items = @($FunResult)  # normalisiert null/single/multi
+
+    if ($items.Count -eq 0) {
+        $FallbacktoSSH = $true
+    }
+    # optional: wenn es wirklich ein "Einzelobjekt mit StorageInfo" ist:
+    elseif ($items.Count -eq 1 -and ($items[0].PSObject.Properties.Name -contains 'StorageInfo')) {
+        if ($null -eq $items[0].StorageInfo -or [string]::IsNullOrWhiteSpace([string]$items[0].StorageInfo.ID)) {
+            $FallbacktoSSH = $true
+        }
     }
 
-    # 2) DeviceToggle bauen
-    $DeviceIdent = [DeviceToggle]::new()
-    $DeviceIdent.Id = "DeviceBlock$($Device.ID)"
+        if ($FallbacktoSSH) {
+            $FunResult = & $SSHFunc -TD_Line_ID $Device.ID -TD_Device_ConnectionTyp $Device.ConnectionTyp -TD_Device_UserName $Device.UserName -TD_Device_DeviceIP $Device.IPAddress -TD_Device_PW $pw -TD_Exportpath $ExportPath
+        }
 
-    # Label robust: ClusterName kann je nach Result-Shape anders sein
-    $cluster = $FunResult.ClusterName
-    $DeviceIdent.Label = if ([string]::IsNullOrWhiteSpace([string]$cluster)) { "$($Device.IPAddress)" } else { "$cluster" }
+        # 2) DeviceToggle bauen
+        $DeviceIdent = [DeviceToggle]::new()
+        $DeviceIdent.Id = "DeviceBlock$($Device.ID)"
 
-    $DeviceIdent.IsChecked = $false
-    return @{ DeviceIdent = $DeviceIdent; FuncResult = $FunResult }
+        # Label robust: ClusterName kann je nach Result-Shape anders sein
+        $cluster = $FunResult.ClusterName
+        $DeviceIdent.Label = if ([string]::IsNullOrWhiteSpace([string]$cluster)) { "$($Device.IPAddress)" } else { "$cluster" }
+
+        $DeviceIdent.IsChecked = $false
+        return @{ DeviceIdent = $DeviceIdent; FuncResult = $FunResult }
+}
+
+#c&p need for doubel view, is a bit diff as New-devBlock
+function RestThenSshForCombiView {
+    param(
+        [Parameter(Mandatory)]
+        $Device,
+        [string]$ExportPath,
+        [Parameter(Mandatory)]
+        [object]$RESTFunc,
+        [Parameter(Mandatory)]
+        [object]$SSHFunc
+    )
+
+    [bool]$FallbacktoSSH = $false
+    # 1) Daten holen (REST -> wenn leer -> SSH) StorageInfo
+    $pw = [Net.NetworkCredential]::new('', $Device.Password).Password
+    
+    $FunResult = & $RESTFunc -TD_Line_ID $Device.ID -TD_Device_UserName $Device.UserName -TD_Device_DeviceIP $Device.IPAddress -TD_Device_PW $pw -TD_Exportpath $ExportPath
+
+    $items = @($FunResult)  # normalisiert null/single/multi
+
+    if ($items.Count -eq 0) {
+        $FallbacktoSSH = $true
+    }
+    # optional: wenn es wirklich ein "Einzelobjekt mit StorageInfo" ist:
+    elseif ($items.Count -eq 1 -and ($items[0].PSObject.Properties.Name -contains 'StorageInfo')) {
+        if ($null -eq $items[0].StorageInfo -or [string]::IsNullOrWhiteSpace([string]$items[0].StorageInfo.ID)) {
+            $FallbacktoSSH = $true
+        }
+    }
+
+    if ($FallbacktoSSH) {
+        $FunResult = & $SSHFunc -TD_Line_ID $Device.ID -TD_Device_ConnectionTyp $Device.ConnectionTyp -TD_Device_UserName $Device.UserName -TD_Device_DeviceIP $Device.IPAddress -TD_Device_PW $pw -TD_Exportpath $ExportPath
+    }
+  
+    return $FunResult
 }
 
 # Generischer Mapper: Add-Rows
@@ -81,4 +132,18 @@ function Add-MappedRows {
         }
         $Collection.Add([pscustomobject]$h) | Out-Null
     }
+}
+
+# SpectrumTimestamp
+function Convert-SpectrumTimestamp {
+    param(
+        [Parameter(Mandatory)]
+        [string]$Timestamp
+    )
+
+    return [datetime]::ParseExact(
+        $Timestamp,
+        "yyMMddHHmmss",
+        [System.Globalization.CultureInfo]::InvariantCulture
+    )
 }
