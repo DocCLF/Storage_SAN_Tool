5a5573f432179be96a7c9f7945a26de9fa4dd2b4
ToolDoc <mailtodoc@web.de>
2026-01-27 13:26:35 +0100

    Update SST_InvokeFOSRestPWSH.ps1

--- TOOLFunc/REST/SST_InvokeFOSRestPWSH.ps1
+++ TOOLFunc/REST/SST_InvokeFOSRestPWSH.ps1
@@ -98,7 +98,7 @@ function SST_InvokeFOSRestPWSH {
   # Build URL (+ optional vf_id)
   $uri = "$base/rest/running/$resource"
   if ($PSBoundParameters.ContainsKey('VfId')) {
-    $join = ($uri -match '\?') ? '&' : '?'
+    $join = if ($uri -match '\?') { '&' } else { '?' }
     $uri = "$uri$join" + "vf_id=$VfId"
   }
 
