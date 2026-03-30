2dc87f43dae2da3bb4c77f1d3682afb501a8b790
ToolDoc <mailtodoc@web.de>
2026-03-30 10:18:46 +0200

    Update Storage_SAN_Tool.psm1

--- Storage_SAN_Tool.psm1
+++ Storage_SAN_Tool.psm1
@@ -28,6 +28,8 @@ By default, these files are blocked to protect the computer from untrusted files
     $IBMStorage_SSHFunctions = @(Get-ChildItem -Path $PSScriptRoot\Storage\IBM\SSH\*.ps1 -ErrorAction SilentlyContinue)
     Unblock-File -Path $PSScriptRoot\SAN\Brocade\SSH\*.ps1 -Confirm:$false
     $FOSBrocade_Functions = @(Get-ChildItem -Path $PSScriptRoot\SAN\Brocade\SSH\*.ps1 -ErrorAction SilentlyContinue)
+    Unblock-File -Path $PSScriptRoot\Tape\*.ps1 -Confirm:$false
+    $IBMTape_Functions = @(Get-ChildItem -Path $PSScriptRoot\Tape\*.ps1 -ErrorAction SilentlyContinue)
     Unblock-File -Path $PSScriptRoot\HealthCheck\*.ps1 -Confirm:$false
     $HealthCheck_Functions = @(Get-ChildItem -Path $PSScriptRoot\HealthCheck\*.ps1 -ErrorAction SilentlyContinue)
     Unblock-File -Path $PSScriptRoot\GUI\*.ps1 -Confirm:$false
@@ -44,7 +46,7 @@ By default, these files are blocked to protect the computer from untrusted files
             }
         }
 
-        foreach($import in @($REST_Functions + $GUIBasics_Functions + $DBFunc_Functions + $PRISIMFunc_Functions + $DashBoard_Functions + $TOOL_Functions + $USER_Functions + $IBMPower_Functions + $IBMStorage_SSHFunctions + $IBMStorage_RESTFunctions + $FOSBrocade_Functions + $HealthCheck_Functions + $GUI_Functions)) {
+        foreach($import in @($REST_Functions + $GUIBasics_Functions + $DBFunc_Functions + $IBMTape_Functions +$PRISIMFunc_Functions + $DashBoard_Functions + $TOOL_Functions + $USER_Functions + $IBMPower_Functions + $IBMStorage_SSHFunctions + $IBMStorage_RESTFunctions + $FOSBrocade_Functions + $HealthCheck_Functions + $GUI_Functions)) {
             try {
                . $import.fullname
             }
