c1086b9397a7176fba8466ef26489efe2224c442
ToolDoc <mailtodoc@web.de>
2026-04-16 09:58:57 +0200

    Update SST_DashBoardEvents.ps1

--- TOOLFunc/DashBoard/SST_DashBoardEvents.ps1
+++ TOOLFunc/DashBoard/SST_DashBoardEvents.ps1
@@ -1,32 +1,49 @@
 function SST_DashBoardEvents {
     [CmdletBinding()]
     param (
-        $STOEVCollection
+        [Parameter(Mandatory)]
+        [string]$Query,
+        [Parameter(Mandatory)]
+        $SQLConnection,
+        [int]$EventCounter = 0
     )
     
-    begin {
-        
-    }
-    
-    process {
-        # SeqID,LastTime,ObjectType,ObjectID,ObjectName,CopyID,Status,Fixed,ErrorCode,Description,WWNN,SerialNumber
-        [int]$EventCounter = 0
-        while ($STOEVCollection.Read()) {
-
+    try {
+        $DashBoardEventsView = [System.Collections.Generic.List[object]]::new()
+        $SQLConnection.Open()
+        $SQLiteCommand = $SQLConnection.CreateCommand()
+        $SQLiteCommand.CommandText = $Query
+        $SST_SQLiteDBReader = $SQLiteCommand.ExecuteReader()
+        #ID, SerialNumber, ObjectName, Status, ErrorCode, Description, TimeStamp
+        while ($SST_SQLiteDBReader.Read()) {
             $EventCounter++
-
-            
+            $DashBoardEventsObj = [PSCustomObject]@{
+                SerialNumber  = $SST_SQLiteDBReader["SerialNumber"]
+                ObjectName  = $SST_SQLiteDBReader["ObjectName"]
+                Status = $SST_SQLiteDBReader["Status"]
+                ErrorCode = $SST_SQLiteDBReader["ErrorCode"]
+                Description = $SST_SQLiteDBReader["Description"]
+                TimeStamp    = $SST_SQLiteDBReader["TimeStamp"]
+            }
+            $DashBoardEventsView.Add($DashBoardEventsObj)
         }
-        #Write-Host $EventCounter
+
         $TD_TB_STOEventsCount.Text = $EventCounter
         if($EventCounter -ge 1){
             $TD_TB_HealthStatus.Text = "Attention"
             $TD_TB_HealthStatus.Foreground = "Orange"
             $TD_TB_STOEventsCount.Foreground = "Orange"
         }
+        return $DashBoardEventsView
+    }
+    catch {
+        Write-Host $_.Exception.Message
+    }
+    finally{
+        #if ($SST_SQLiteDBReader) { $SST_SQLiteDBReader.Close() }
+        if ($SQLiteCommand) { $SQLiteCommand.Dispose() }
+        if ($SQLConnection.State -eq 'Open') { $SQLConnection.Close() }
+        #$SQLConnection.Dispose()
     }
     
-    end {
-        if ($reader) { $reader.Close() }
-    }
 }
