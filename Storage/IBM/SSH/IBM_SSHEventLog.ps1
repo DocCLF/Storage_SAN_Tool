e91e21c41ac41a034626a49c63421eff44d08ef0
ToolDoc <mailtodoc@web.de>
2026-04-17 14:06:48 +0200

    Update IBM_SSHEventLog.ps1

--- Storage/IBM/SSH/IBM_SSHEventLog.ps1
+++ Storage/IBM/SSH/IBM_SSHEventLog.ps1
@@ -96,7 +96,7 @@ function IBM_SSHEventLog {
     
     end {
         Close-ProgressBar -ProgressBar $ProgressBar
-        SST_CustomerDeviceDBInsertTable -SST_InfoType "StorageEventLog" -SST_CollectedInformations $TD_EventCollection
+        SST_CustomerSTODBInsertTable -SST_InfoType "StorageEventLog" -SST_CollectedInformations $TD_EventCollection
 
         <# export y or n #>
         if($TD_Export -eq "yes"){
