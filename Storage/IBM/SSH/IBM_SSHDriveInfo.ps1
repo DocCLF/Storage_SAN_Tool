758f13dfb16e6d0c1e16c0f48172ccffef4f58f6
ToolDoc <mailtodoc@web.de>
2026-04-17 14:06:51 +0200

    Update IBM_SSHDriveInfo.ps1

--- Storage/IBM/SSH/IBM_SSHDriveInfo.ps1
+++ Storage/IBM/SSH/IBM_SSHDriveInfo.ps1
@@ -163,7 +163,7 @@ function IBM_SSHDriveInfo {
 
     end{
         Close-ProgressBar -ProgressBar $ProgressBar
-        SST_CustomerDeviceDBInsertTable -SST_InfoType "StorageDrive" -SST_CollectedInformations $TD_DriveOverview
+        SST_CustomerSTODBInsertTable -SST_InfoType "StorageDrive" -SST_CollectedInformations $TD_DriveOverview
         
         <# export y or n #>
         if($TD_export -eq "yes"){
