c7bb2caca3c34e7d15a07cffacdbd1e5de1f6cc2
ToolDoc <mailtodoc@web.de>
2026-04-17 14:07:04 +0200

    Update IBM_RESTHostInfo.ps1

--- Storage/IBM/REST/IBM_RESTHostInfo.ps1
+++ Storage/IBM/REST/IBM_RESTHostInfo.ps1
@@ -106,7 +106,7 @@ function IBM_RESTHostInfo {
     
     end {
         Close-ProgressBar -ProgressBar $ProgressBar
-        SST_CustomerDeviceDBInsertTable -SST_InfoType "StorageHostInfo" -SST_CollectedInformations $CollectedHostInfo
+        SST_CustomerSTODBInsertTable -SST_InfoType "StorageHostInfo" -SST_CollectedInformations $CollectedHostInfo
         <# export y or n #>
         if($TD_Export -eq "yes"){
             if([string]$TD_Exportpath -ne "$PSCommandPath\ToolLog\"){
