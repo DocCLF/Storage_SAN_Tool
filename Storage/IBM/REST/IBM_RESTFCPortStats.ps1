1bdb6f126b470152c19ab384cfed641715426547
ToolDoc <mailtodoc@web.de>
2026-04-17 14:07:08 +0200

    Update IBM_RESTFCPortStats.ps1

--- Storage/IBM/REST/IBM_RESTFCPortStats.ps1
+++ Storage/IBM/REST/IBM_RESTFCPortStats.ps1
@@ -86,7 +86,7 @@ function IBM_RESTFCPortStats {
 
     end {
         Close-ProgressBar -ProgressBar $ProgressBar
-        SST_CustomerDeviceDBInsertTable -SST_InfoType "FCPortStats" -SST_CollectedInformations $TD_PortStats_Overview
+        SST_CustomerSTODBInsertTable -SST_InfoType "FCPortStats" -SST_CollectedInformations $TD_PortStats_Overview
         <# export y or n #>
         if($TD_export -eq "yes"){
 
