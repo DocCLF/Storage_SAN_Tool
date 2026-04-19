3251fcadf8a5e016a7538f03b70b9fe68307a48f
ToolDoc <mailtodoc@web.de>
2026-04-19 19:51:31 +0200

    Update SST_GetHMCPowerToken.ps1

--- TOOLFunc/REST/SST_GetHMCPowerToken.ps1
+++ TOOLFunc/REST/SST_GetHMCPowerToken.ps1
@@ -106,7 +106,7 @@ function SST_GetHMCPowerToken {
                 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
             }
 
-            $result = Invoke-WebRequest @InvokeWebReqParamsBlock -UseBasicParsing
+            $result = Invoke-WebRequest @InvokeWebReqParamsBlock
         }
         finally {
             if ($IgnoreCertificate) {
