f7a798216105a4cc80fc5c8ba543f7a16d77d4cb
ToolDoc <mailtodoc@web.de>
2026-05-04 19:44:25 +0200

    Update IBM_RESTVDiskAnalysis.ps1

--- Storage/IBM/REST/IBM_RESTVDiskAnalysis.ps1
+++ Storage/IBM/REST/IBM_RESTVDiskAnalysis.ps1
@@ -46,6 +46,8 @@ function IBM_RESTVDiskAnalysis {
     process {
         [int]$imax = $TD_DeviceInformation.Count
         [array]$TD_VDiskFuncResault = for ($i = 0; $i -lt $imax; $i++) {
+            <# Max requests/sec to command endpoints = 10 -.- #>
+            if ($i % 8 -eq 0) { Start-Sleep -Milliseconds 1500 }
             <# Node Info#>
             $TD_VDiskinfo = "" | Select-Object ID,Name,State,AnalysisTime,Capacity,ThinSize,ThinSavings,ThinSavingsRatio,CompressedSize,CompressionSavings,CompressionSavingsRatio,TotalSavings,TotalSavingsRatio,MarginOfError,WWNN,SerialNumber
 
