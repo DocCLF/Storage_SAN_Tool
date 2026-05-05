ce39fbcefb095d88b2e28eca48aa796e8edd95e0
ToolDoc <mailtodoc@web.de>
2026-05-05 15:49:08 +0200

    Update FOS_SSHPortbufferShowInfo.ps1

--- SAN/Brocade/SSH/FOS_SSHPortbufferShowInfo.ps1
+++ SAN/Brocade/SSH/FOS_SSHPortbufferShowInfo.ps1
@@ -116,10 +116,10 @@ function FOS_SSHPortbufferShowInfo {
             <# exported to .\Host_Volume_Map_Result.csv #>
             if([string]$TD_Exportpath -ne "$PSRootPath\ToolLog\"){
                 $FOS_pbs | Export-Csv -Path $TD_Exportpath\$($TD_Line_ID)_$($TD_Device_DeviceName)_PortBufferShow_Result_$(Get-Date -Format "yyyy-MM-dd").csv -NoTypeInformation
-                #SST_ToolMessageCollector -TD_ToolMSGCollector "$TD_Exportpath\$($TD_Line_ID)_$($TD_Device_DeviceName)_PortBufferShow_Result_$(Get-Date -Format "yyyy-MM-dd").csv" -TD_ToolMSGType Debug
+                SST_ToolMessageCollector -TD_ToolMSGCollector "$TD_Exportpath\$($TD_Line_ID)_$($TD_Device_DeviceName)_PortBufferShow_Result_$(Get-Date -Format "yyyy-MM-dd").csv" -TD_ToolMSGType Debug
             }else {
                 $FOS_pbs | Export-Csv -Path $PSScriptRoot\ToolLog\$($TD_Line_ID)_$($TD_Device_DeviceName)_PortBufferShow_Result_$(Get-Date -Format "yyyy-MM-dd").csv -NoTypeInformation
-                #SST_ToolMessageCollector -TD_ToolMSGCollector "$PSScriptRoot\ToolLog\$($TD_Line_ID)_$($TD_Device_DeviceName)_PortBufferShow_Result_$(Get-Date -Format "yyyy-MM-dd").csv" -TD_ToolMSGType Debug
+                SST_ToolMessageCollector -TD_ToolMSGCollector "$PSScriptRoot\ToolLog\$($TD_Line_ID)_$($TD_Device_DeviceName)_PortBufferShow_Result_$(Get-Date -Format "yyyy-MM-dd").csv" -TD_ToolMSGType Debug
             }
         }else {
             <# output on the promt #>
