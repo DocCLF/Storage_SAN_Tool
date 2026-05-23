acaef92afd42d89686d48500e4941d1de31bc48a
ToolDoc <mailtodoc@web.de>
2026-05-23 18:19:07 +0200

    Create Get-BrocadeSensorOverview.ps1

---
+++ SAN/Brocade/REST/Get-BrocadeSensorOverview.ps1
@@ -0,0 +1,55 @@
+function Get-BrocadeSensorOverview {
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        $Device
+    )
+
+    $TemperatureInfo = Get-BrocadeTemperatureInfo -Device $Device
+    $Fans = @(Get-BrocadeFanInfo -Device $Device)
+    $PowerSupplies = @(Get-BrocadePowerSupplyInfo -Device $Device)
+
+    $FailedFans = @(
+        $Fans | Where-Object { -not $_.IsHealthy }
+    ).Count
+
+    $FailedPowerSupplies = @(
+        $PowerSupplies | Where-Object { -not $_.IsHealthy }
+    ).Count
+
+    $HotSensors = if($TemperatureInfo){
+        [int]$TemperatureInfo.HotSensors
+    }
+    else{
+        0
+    }
+
+    [PSCustomObject]@{
+    TemperatureInfo = $TemperatureInfo
+    TemperatureAverage = $TemperatureInfo.AverageTemp
+    TemperatureMax = $TemperatureInfo.MaxTemp
+    TemperatureMin = $TemperatureInfo.MinTemp
+    TemperatureHealth = $TemperatureInfo.HealthState
+        Fans = $Fans
+        PowerSupplies = $PowerSupplies
+
+        FanCount = $Fans.Count
+        PSUCount = $PowerSupplies.Count
+
+        FailedFans = $FailedFans
+        FailedPowerSupplies = $FailedPowerSupplies
+        HotSensors = $HotSensors
+
+        OverallHealth = if(
+            $FailedFans -gt 0 -or
+            $FailedPowerSupplies -gt 0 -or
+            $HotSensors -gt 0
+        ){
+            'Warning'
+        }
+        else{
+            'OK'
+        }
+    }
+}
