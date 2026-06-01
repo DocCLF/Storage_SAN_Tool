8465e6f6a9a77a238290434f884064ecf4eec89a
ToolDoc <mailtodoc@web.de>
2026-06-01 08:28:49 +0200

    Update Get-BrocadeEffectiveZoneShow.ps1

--- SAN/Brocade/REST/Get-BrocadeEffectiveZoneShow.ps1
+++ SAN/Brocade/REST/Get-BrocadeEffectiveZoneShow.ps1
@@ -108,7 +108,7 @@ function Get-BrocadeEffectiveZoneShow {
         }
     }
     try {
-        $FOS_ZoneCollection | Export-Csv -Path $($TD_TB_ExportPath.Text)\ZoneShow_$($EffectiveResBase.'cfg-name')_$(Get-Date -Format "yyyy-MM-dd").csv -NoTypeInformation
+        $FOS_ZoneCollection | Export-Csv -Path "$($TD_TB_ExportPath.Text)\ZoneShow_$($EffectiveResBase.'cfg-name')_$(Get-Date -Format "yyyy-MM-dd").csv" -NoTypeInformation
     }
     catch {
         SST_ToolMessageCollector -TD_ToolMSGCollector "ZoneShow: $($_.Exception.Message)" -TD_ToolMSGType "Warning" -TD_Shown "no"
