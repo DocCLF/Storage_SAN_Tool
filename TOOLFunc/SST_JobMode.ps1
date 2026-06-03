07fa8122479b7393167054d9ddaba6e8fd8c6af3
ToolDoc <mailtodoc@web.de>
2026-06-03 08:06:18 +0200

    Update SST_JobMode.ps1

--- TOOLFunc/SST_JobMode.ps1
+++ TOOLFunc/SST_JobMode.ps1
@@ -29,7 +29,7 @@ function SST_JobMode {
                 catch {
                     <#Do this if a terminating exception happens#>
                     Write-Host $_.exception.message
-                    SST_ToolMessageCollector -TD_ToolMSGCollector "CockPit JobMode Drive - $_.exception.message" -TD_ToolMSGType Error -TD_Shown no
+                    SST_ToolMessageCollector -TD_ToolMSGCollector "CockPit JobMode Storage Drive - $_.exception.message" -TD_ToolMSGType Error -TD_Shown no
                 }
                 <#Storage Host Infos#>
                 try {
@@ -40,17 +40,57 @@ function SST_JobMode {
                     Write-Host $_.exception.message
                     SST_ToolMessageCollector -TD_ToolMSGCollector "CockPit JobMode Storage Host - $($_.exception.message)" -TD_ToolMSGType Error -TD_Shown no
                 }
+                <#Storage EventLog Infos#>
+                try {
+                    Invoke-DeviceDataFetch -Device $Device -ExportPath $ExportPath -RESTFunc IBM_RESTEventLog -SSHFunc IBM_SSHEventLog | Out-Null
+                }
+                catch {
+                    <#Do this if a terminating exception happens#>
+                    Write-Host $_.exception.message
+                    SST_ToolMessageCollector -TD_ToolMSGCollector "CockPit JobMode Storage Event Log - $($_.exception.message)" -TD_ToolMSGType Error -TD_Shown no
+                }
             }
             <# SAN Area #>
             if($_.DeviceTyp -like "*SAN*"){
                 $Device = $_
                 try {
-                    Invoke-DeviceDataFetch -Device $Device -ExportPath $ExportPath -RESTFunc IBM_RESTHostInfo -SSHFunc IBM_SSHHostInfo | Out-Null
+                    Invoke-DeviceDataFetch -Device $Device -ExportPath $ExportPath -RESTFunc Get-BrocadeBaseInfo -SSHFunc FOS_SSHBasicSwitchInfos | Out-Null
                 }
                 catch {
                     <#Do this if a terminating exception happens#>
                     SST_ToolMessageCollector -TD_ToolMSGCollector "CockPit JobMode SAN Base - $($_.exception.message)" -TD_ToolMSGType Error -TD_Shown no
                 }
+            }
+            if($_.DeviceTyp -like "*Tape*"){
+                $Device = $_
+                try {
+                    $LibBaseInfo = Invoke_IBMTapeLibraryApi -Device $Device -Endpoint 'library/baseinfo'
+                    Start-Sleep -Seconds 1
+                    $LibInfo = Invoke_IBMTapeLibraryApi -Device $Device -Endpoint 'library'
+                    Start-Sleep -Seconds 1
+                    $MergeLibObj = Merge-PSCustomObject -InputObject @($($LibBaseInfo.BaseInfo), $LibInfo)
+                    SST_CustomerLibraryDBInsertTable -SST_InfoType "LibraryBaseInfo" -SST_CollectedInformations $MergeLibObj
+                }
+                catch {
+                    <#Do this if a terminating exception happens#>
+                    SST_ToolMessageCollector -TD_ToolMSGCollector "CockPit JobMode Tape Base - $($_.exception.message)" -TD_ToolMSGType Error -TD_Shown no
+                }
+
+            }
+            if($_.DeviceTyp -like "*Power*"){
+                $Device = $_
+                try {
+                    New-DeviceBlock -Device $Device -ExportPath $TD_TB_ExportPath.Text -RESTFunc HMC_RESTHMCConsole | Out-Null
+                    Start-Sleep -Seconds 1
+                    New-DeviceBlock -Device $Device -ExportPath $TD_TB_ExportPath.Text -RESTFunc HMC_RESTHMCManagedSystems | Out-Null
+                    Start-Sleep -Seconds 1
+                    New-DeviceBlock -Device $Device -ExportPath $TD_TB_ExportPath.Text -RESTFunc HMC_RESTHMCLogicalPartitions | Out-Null
+                }
+                catch {
+                    <#Do this if a terminating exception happens#>
+                    SST_ToolMessageCollector -TD_ToolMSGCollector "CockPit JobMode Power - $($_.exception.message)" -TD_ToolMSGType Error -TD_Shown no
+                }
+
             }
         }
     }
