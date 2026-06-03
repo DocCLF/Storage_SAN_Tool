5c4ee9665a8d6714858386d4c37b5a5944688561
ToolDoc <mailtodoc@web.de>
2026-06-03 12:26:56 +0200

    Update IBM_SANHealthCheck.ps1

--- HealthCheck/IBM_SANHealthCheck.ps1
+++ HealthCheck/IBM_SANHealthCheck.ps1
@@ -29,19 +29,19 @@ function IBM_SANHealthCheck {
                         SST_CreateHealthLayout -SST_UCOBJ $UCOBJ -SST_MainStackPName "$BROSANDeviceMainSTPName" -SST_GridFuncName "BROSANBaseInfoFunc$($_.ID)" -SST_StackPFuncName "FuncBROSANBaseInfoStackPN$($_.ID)" -SST_LabelVisuNameofCheck "SwitchInfo" -SST_StackPResultsName "ResultsBROSANBaseInfoStackPN$($_.ID)" -SST_DeviceID $_.ID -SST_MainStackPWith $MainWith -DeviceIP $_.IPAddress
                         
                         #region SANSwitch_Base_Info
-                        $FOS_BasicSwitch = FOS_BasicSwitchInfos -TD_Line_ID $_.ID -TD_Device_ConnectionTyp $_.ConnectionTyp -TD_Device_UserName $_.UserName -TD_Device_DeviceIP $_.IPAddress -TD_Device_DeviceName $_.DeviceName -TD_Device_PW $([Net.NetworkCredential]::new('', $_.Password).Password) 
+                        $FOS_BasicSwitch = Get-BrocadeBaseInfo -Device $SST_DeviceLoggingInfo
                         SST_ToolMessageCollector -TD_ToolMSGCollector "SANSwitch_Base_Info" -TD_ToolMSGType Debug -TD_Shown no
                         [int]$i=0
-                        SST_CreateHealthLayout -SST_UCOBJ $UCOBJ -SST_MainStackPName "$BROSANDeviceMainSTPName" -SST_GridFuncName "BROSANBaseInfoFunc$DeviceIDPlaceHolder" -SST_LabelColorForCheck "green" -SST_StackPFuncName "FuncBROSANBaseInfoStackPN$DeviceIDPlaceHolder" -SST_StackPResultsName "ResultsBROSANBaseInfoStackPN$DeviceIDPlaceHolder" -SST_LabelNameHelper "$("BROSANBaseInfoCheck$DeviceIDPlaceHolder"+"_"+$i)" -SST_DeviceID $DeviceIDPlaceHolder -SST_LabelVisuNameofCheck "SwitchInfo" -DataGridSecOption $true -SST_MainStackPWith $MainWith
+                        SST_CreateHealthLayout -SST_UCOBJ $UCOBJ -SST_MainStackPName "$BROSANDeviceMainSTPName" -SST_GridFuncName "BROSANBaseInfoFunc$DeviceIDPlaceHolder" -SST_LabelVisuResultsofCheck $("Status: Done ") -SST_LabelColorForCheck "green" -SST_StackPFuncName "FuncBROSANBaseInfoStackPN$DeviceIDPlaceHolder" -SST_StackPResultsName "ResultsBROSANBaseInfoStackPN$DeviceIDPlaceHolder" -SST_LabelNameHelper "$("BROSANBaseInfoCheck$DeviceIDPlaceHolder"+"_"+$i)" -SST_DeviceID $DeviceIDPlaceHolder -SST_LabelVisuNameofCheck "SwitchInfo" -SST_MainStackPWith $MainWith
                         $DGforKeyValueStatusInfoText = $UCOBJ.FindName("DGforKeyValueSwitchInfoStatusInfoText$DeviceIDPlaceHolder")
-                        $DGforKeyValueStatusInfoText.ItemsSource = $FOS_BasicSwitch
-                        
+                        $DGforKeyValueStatusInfoText.ItemsSource = @($FOS_BasicSwitch)
+
                         $i=0
                         $UCOBJ.Dispatcher.Invoke([System.Action]{},"Render")
                         #endregion
 
                         #region FOS_PortbufferShowInfo
-                        [array]$FOS_PortbufferShowInfo = FOS_PortbufferShowInfo -TD_Line_ID $_.ID -TD_Device_ConnectionTyp $_.ConnectionTyp -TD_Device_UserName $_.UserName -TD_Device_DeviceIP $_.IPAddress -TD_Device_DeviceName $_.DeviceName -TD_Device_PW $([Net.NetworkCredential]::new('', $_.Password).Password) 
+                        [array]$FOS_PortbufferShowInfo = Get-BrocadePortBufferStats -Device $SST_DeviceLoggingInfo 
                         SST_ToolMessageCollector -TD_ToolMSGCollector "FOS_PortbufferShowInfo" -TD_ToolMSGType Debug -TD_Shown no
 
                         [int]$i=0
@@ -54,7 +54,7 @@ function IBM_SANHealthCheck {
                         #endregion
 
                         #region FOS_PortErrShowInfos
-                        [array]$FOS_PortErrShowInfos = FOS_PortErrShowInfos -TD_Line_ID $_.ID -TD_Device_ConnectionTyp $_.ConnectionTyp -TD_Device_UserName $_.UserName -TD_Device_DeviceIP $_.IPAddress -TD_Device_DeviceName $_.DeviceName -TD_Device_PW $([Net.NetworkCredential]::new('', $_.Password).Password) 
+                        [array]$FOS_PortErrShowInfos = Get-BrocadePortErrorStats -Device $SST_DeviceLoggingInfo 
                         SST_ToolMessageCollector -TD_ToolMSGCollector "FOS_PortErrShowInfos" -TD_ToolMSGType Debug -TD_Shown no
 
                         if(!([String]::IsNullOrEmpty($FOS_PortErrShowInfos))){
@@ -67,7 +67,7 @@ function IBM_SANHealthCheck {
                         #endregion
 
                         #region FOS_PortLicenseShowInfo
-                        [array]$FOS_PortLicenseShowInfo = FOS_PortLicenseShowInfo -TD_Line_ID $_.ID -TD_Device_ConnectionTyp $_.ConnectionTyp -TD_Device_UserName $_.UserName -TD_Device_DeviceIP $_.IPAddress -TD_Device_DeviceName $_.DeviceName -TD_Device_PW $([Net.NetworkCredential]::new('', $_.Password).Password) 
+                        [array]$FOS_PortLicenseShowInfo = Get-BrocadeLicenseOverview -Device $SST_DeviceLoggingInfo 
                         SST_ToolMessageCollector -TD_ToolMSGCollector "FOS_PortLicenseShowInfo" -TD_ToolMSGType Debug -TD_Shown no
                         
                         if(!([String]::IsNullOrEmpty($FOS_PortLicenseShowInfo))){
@@ -77,7 +77,7 @@ function IBM_SANHealthCheck {
                         #endregion
 
                         #region FOS_SensorShow
-                        [array]$FOS_SensorShow   = FOS_SensorShow -TD_Line_ID $_.ID -TD_Device_ConnectionTyp $_.ConnectionTyp -TD_Device_UserName $_.UserName -TD_Device_DeviceIP $_.IPAddress -TD_Device_DeviceName $_.DeviceName -TD_Device_PW $([Net.NetworkCredential]::new('', $_.Password).Password) 
+                        [array]$FOS_SensorShow = Get-BrocadeSensorOverview -Device $SST_DeviceLoggingInfo 
                         SST_ToolMessageCollector -TD_ToolMSGCollector "FOS_SensorShow" -TD_ToolMSGType Debug -TD_Shown no
                         
                         if(!([String]::IsNullOrEmpty($FOS_SensorShow))){
@@ -87,17 +87,20 @@ function IBM_SANHealthCheck {
                         #endregion
 
                         #region FOS_SFPDetails
-                        [array]$FOS_SFPDetails = FOS_SFPDetails -TD_Line_ID $_.ID -TD_Device_ConnectionTyp $_.ConnectionTyp -TD_Device_UserName $_.UserName -TD_Device_DeviceIP $_.IPAddress -TD_Device_DeviceName $_.DeviceName -TD_Device_PW $([Net.NetworkCredential]::new('', $_.Password).Password) 
+                        [array]$FOS_SFPDetails = Get-BrocadeSFPShow -Device $SST_DeviceLoggingInfo 
                         SST_ToolMessageCollector -TD_ToolMSGCollector "FOS_SFPDetails" -TD_ToolMSGType Debug -TD_Shown no
                         [int]$i=0
+                        Write-Host $FOS_SFPDetails
                         $FOS_SFPDetails |ForEach-Object {
                             $i++
-                            if(($_.HealthStatus -eq "Yellow") -or ([string]::IsNullOrWhiteSpace($_.HealthStatus)) -or ($_.HealthStatus -eq "Unknown") -or ($_.HealthStatus -like "No License")){
+                            if(($_.OpticalState -eq "Low Signal") -or ($_.OpticalState -eq "Unknown")){
                                 <# Create a InfoLabel with the MSG and push the color for the Device#>
-                                SST_CreateHealthLayout -SST_UCOBJ $UCOBJ -SST_MainStackPName "$BROSANDeviceMainSTPName" -SST_GridFuncName "BROSANSFPDetailsFunc$DeviceIDPlaceHolder" -SST_LabelVisuResultsofCheck $("Port: $($_.Port)"+" - "+"SFPTyp: $($_.SFPTyp)"+" - "+"Speed: $($_.SpeedRange)"+" - "+"HealthStatus: $($_.HealthStatus)") -SST_LabelColorForCheck "Yellow" -SST_StackPFuncName "FuncBROSANSFPDetailsStackPN$DeviceIDPlaceHolder" -SST_StackPResultsName "ResultsBROSANSFPDetailsStackPN$DeviceIDPlaceHolder" -SST_LabelNameHelper "$("BROSANSFPDetailsCheck$DeviceIDPlaceHolder"+"_"+$i)" -SST_DeviceID $DeviceIDPlaceHolder -SST_LabelVisuNameofCheck "SFPDetails" -SST_MainStackPWith $MainWith
+                                SST_CreateHealthLayout -SST_UCOBJ $UCOBJ -SST_MainStackPName "$BROSANDeviceMainSTPName" -SST_GridFuncName "BROSANSFPDetailsFunc$DeviceIDPlaceHolder" -SST_LabelVisuResultsofCheck $("Port: $($_.Port)"+" - "+"SFPTyp: $($_.SFPTyp)"+" - "+"Speed: $($_.SpeedRange)"+" - "+"HealthStatus: $($_.OpticalState)") -SST_LabelColorForCheck "Yellow" -SST_StackPFuncName "FuncBROSANSFPDetailsStackPN$DeviceIDPlaceHolder" -SST_StackPResultsName "ResultsBROSANSFPDetailsStackPN$DeviceIDPlaceHolder" -SST_LabelNameHelper "$("BROSANSFPDetailsCheck$DeviceIDPlaceHolder"+"_"+$i)" -SST_DeviceID $DeviceIDPlaceHolder -SST_LabelVisuNameofCheck "SFPDetails" -SST_MainStackPWith $MainWith
+                            }elseif($_.OpticalState -like "OK"){
+                                SST_CreateHealthLayout -SST_UCOBJ $UCOBJ -SST_MainStackPName "$BROSANDeviceMainSTPName" -SST_GridFuncName "BROSANSFPDetailsFunc$DeviceIDPlaceHolder" -SST_LabelVisuResultsofCheck $("Port: $($_.Port)"+" - "+"SFPTyp: $($_.SFPTyp)"+" - "+"Speed: $($_.SpeedRange)"+" - "+"HealthStatus: $($_.OpticalState)") -SST_StackPFuncName "FuncBROSANSFPDetailsStackPN$DeviceIDPlaceHolder" -SST_StackPResultsName "ResultsBROSANSFPDetailsStackPN$DeviceIDPlaceHolder" -SST_LabelNameHelper "$("BROSANSFPDetailsCheck$DeviceIDPlaceHolder"+"_"+$i)" -SST_DeviceID $DeviceIDPlaceHolder -SST_LabelVisuNameofCheck "SFPDetails" -SST_MainStackPWith $MainWith
                             }
                         }
-                        if((($FOS_SFPDetails | Where-Object {$_}).Count) -eq (($FOS_SFPDetails | Where-Object {$_.HealthStatus -eq "Green"}).Count)){
+                        if((($FOS_SFPDetails | Where-Object {$_}).Count) -eq (($FOS_SFPDetails | Where-Object {$_.OpticalState -eq "OK"}).Count)){
                             SST_CreateHealthLayout -SST_UCOBJ $UCOBJ -SST_MainStackPName "$BROSANDeviceMainSTPName" -SST_GridFuncName "BROSANSFPDetailsFunc$DeviceIDPlaceHolder" -SST_LabelVisuResultsofCheck $("Status: Done ") -SST_LabelColorForCheck "green" -SST_StackPFuncName "FuncBROSANSFPDetailsStackPN$DeviceIDPlaceHolder" -SST_StackPResultsName "ResultsBROSANSFPDetailsStackPN$DeviceIDPlaceHolder" -SST_LabelNameHelper "$("BROSANSFPDetailsCheck$DeviceIDPlaceHolder"+"_"+$i)" -SST_DeviceID $DeviceIDPlaceHolder -SST_LabelVisuNameofCheck "SFPDetails" -SST_MainStackPWith $MainWith
                         }
                         $i=0
@@ -105,7 +108,7 @@ function IBM_SANHealthCheck {
                         #endregion
 
                         #region FOS_SwitchShowInfo
-                        [array]$FOS_SwitchShowInfo = FOS_SwitchShowInfo -TD_Line_ID $_.ID -TD_Device_ConnectionTyp $_.ConnectionTyp -TD_Device_UserName $_.UserName -TD_Device_DeviceIP $_.IPAddress -TD_Device_DeviceName $_.DeviceName -TD_Device_PW $([Net.NetworkCredential]::new('', $_.Password).Password) 
+                        [array]$FOS_SwitchShowInfo = Get-BrocadeSwitchShow -Device $SST_DeviceLoggingInfo 
                         SST_ToolMessageCollector -TD_ToolMSGCollector "FOS_SwitchShowInfo" -TD_ToolMSGType Debug -TD_Shown no
 
                         if(!([String]::IsNullOrEmpty($FOS_SwitchShowInfo))){
@@ -115,7 +118,7 @@ function IBM_SANHealthCheck {
                         #endregion
 
                         #region FOS_ZoneDetails
-                        $FOS_ZoneDetails = FOS_ZoneDetails -TD_Line_ID $_.ID -TD_Device_ConnectionTyp $_.ConnectionTyp -TD_Device_UserName $_.UserName -TD_Device_DeviceIP $_.IPAddress -TD_Device_DeviceName $_.DeviceName -TD_Device_PW $([Net.NetworkCredential]::new('', $_.Password).Password) 
+                        $FOS_ZoneDetails = Get-BrocadeEffectiveZoneShow -Device $SST_DeviceLoggingInfo 
                         SST_ToolMessageCollector -TD_ToolMSGCollector "FOS_ZoneDetails" -TD_ToolMSGType Debug -TD_Shown yes
 
                         if(!([String]::IsNullOrEmpty($FOS_ZoneDetails))){
